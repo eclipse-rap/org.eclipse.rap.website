18cf542a17034ac20d7293104d85cb4ab06fc2fa
jkrause <jkrause>
2006-08-12 17:44:06 +0000

    minor updates

--- downloads.php
+++ downloads.php
@@ -6,7 +6,7 @@
 
 	#
 	# Begin: page-specific settings.  Change these. 
-$pageTitle 		= "RAP Project - FAQ";
+$pageTitle 		= "RAP Project - Downloads";
 $pageKeywords	= "AJAX, rap, w4t, eclipse rap";
 $pageAuthor		= "Jochen Krause";
 
