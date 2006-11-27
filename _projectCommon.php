828d668051f30bd840b05ee5e138b0542b410a36
jbohmelo <jbohmelo>
2006-11-27 19:08:01 +0000

    *** empty log message ***

--- _projectCommon.php
+++ _projectCommon.php
@@ -6,6 +6,10 @@
 	# Optional: defaults to system theme 
 	$theme = "";
 
+  require_once($_SERVER['DOCUMENT_ROOT'] . "/projects/common/project-info.class.php");
+  $projectInfo = new ProjectInfo("technology.rap");
+  $projectInfo->generate_common_nav( $Nav );
+
 	# Define your project-wide Nav bars here.
 	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
 	# these are optional
