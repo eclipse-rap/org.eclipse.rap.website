a0d6e85c314261cf36da19a7f4a72efe3013561a
jbohmelo <jbohmelo>
2006-11-27 19:13:46 +0000

    *** empty log message ***

--- _projectCommon.php
+++ _projectCommon.php
@@ -1,22 +1,18 @@
 <?php
-
-	# Set the theme for your project's web pages.
-	# See the Committer Tools "How Do I" for list of themes
-	# https://dev.eclipse.org/committers/
-	# Optional: defaults to system theme 
-	$theme = "";
-
   require_once($_SERVER['DOCUMENT_ROOT'] . "/projects/common/project-info.class.php");
   $projectInfo = new ProjectInfo("technology.rap");
   $projectInfo->generate_common_nav( $Nav );
 
-	# Define your project-wide Nav bars here.
-	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
-	# these are optional
-	$Nav->addNavSeparator("Project Home", 	"index.php");
-	$Nav->addCustomNav("Downloads", 		"downloads.php", 	"_self", 2);
-	$Nav->addCustomNav("CVS", 				"cvs.php", 			"_self", 2);
-#	$Nav->addCustomNav("Installation", 		"install.php", 		"_self", 2);
-	$Nav->addCustomNav("FAQ", 				"faq.php", 			"_self", 2);
-
+	function add2users() {
+	   global $Nav;
+	   $Nav->addCustomNav("FAQ", "/dash/user-faq.php", "", 2);
+		 $Nav->addCustomNav("CVS", "/rap/cvs.php", "", 2);
+		 $Nav->addCustomNav("FAQ", "/rap/faq.php", "", 2);
+	}   
+	
+	function add2users() {
+	   global $Nav;
+	}
+	
+	$projectInfo->generate_common_nav( $Nav, "add2users", NULL, "add2contributors" );
 ?>
