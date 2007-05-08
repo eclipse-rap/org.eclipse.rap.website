3eb396e5c1fe6bfb7ed5561b3deada00a975cef2
rherrmann <rherrmann>
2007-05-08 19:13:16 +0000

    Added hyper-link to ATF

--- faq.php
+++ faq.php
@@ -26,9 +26,9 @@ $html = <<<EOHTML
 		<h1>$pageTitle</h1>
 
 <h3>What is the goal of RAP?</h3>
-<p>It is about enabling programmers to develop powerful, AJAX Web applications
+<p>It is about enabling programmers to develop powerful, Ajax Web applications
 "the eclipse way" - using the plug-in mechanism, a java widget toolkit
-and workbench services. - without having to actually hand-code AJAX.</p>
+and workbench services. - without having to actually hand-code Ajax.</p>
 <h3>Where does RAP play?</h3>
 <p>RAP addresses the same kind of applications like RCP, and complements RCP
 technology with respect to serving as a thin client for distributed
@@ -73,7 +73,7 @@ independently of RCP.</p>
 <p>
 The development model of both toolkits is very similar. There are two main differences: 
 </p><ol>
-<li>GWT is running on an emulated java engine in the browser, RAP is mainly running on the serverside and only smartly refreshing the client by using AJAX. As a result eventhandling in GWT is on the client side (+ possible RPC calls to the server), whereas RAP relays most client-side events to the server for processing (solely ui related events can be processed on the client). </li>
+<li>GWT is running on an emulated java engine in the browser, RAP is mainly running on the serverside and only smartly refreshing the client by using Ajax. As a result eventhandling in GWT is on the client side (+ possible RPC calls to the server), whereas RAP relays most client-side events to the server for processing (solely ui related events can be processed on the client). </li>
 <li>With RAP being mainly server side it can access the full java api and make use of OSGi inside a web container, enabling the full usage of the eclipse plugin model. Applications can contribute to a common workbench and to other plugins. It is not easy to imagine that this can be ported to run in a browser. </li>
 </ol>
 In essence, RAP enables a RCP-comparable approach for web applications, GWT enables a "standalone SWT" comparable approach. 
@@ -85,14 +85,14 @@ applications needs to be rewritten (which means that core plug-ins can be
 reused, as long as they are multi-user capable). It shall be possible to reuse
 Content and Labelproviders.</p>
 <h3>How are RAP and ATF related?</h3>
-<p>RAP is a project focusing on an AJAX web runtime, Ajax Toolkit Framework (ATF
-http://eclipse.org/proposals/atf) focuses on tools to write applications for
-AJAX runtimes. ATP can be used to build components (widgets) for RAP, as RAP is
-encapsulating HTML, CSS and JavaScript into java components.</p>
+<p>RAP is a project focusing on an Ajax web runtime, Ajax Toolkit Framework (
+<a href="http://eclipse.org/atf">ATF</a>) focuses on tools to write applications 
+for Ajax runtimes. ATF can be used to  build components (widgets) for RAP, as 
+RAP is encapsulating HTML, CSS and JavaScript into java components.</p>
 
 <h3>Why don't you use JSF as component framework?</h3>
 <p>Because it is not possible - yet. JSF does currently not offer a generic
-support for AJAX, and although it is possible to use JSF with Java components
+support for Ajax, and although it is possible to use JSF with Java components
 only, there is no implementation available right now.</p>
 <p>We believe that the core achievement of JSF is its Lifecyle handling. W4T
 lifecycle handling is very closely aligned with the JSF lifecycle handling, and
@@ -101,7 +101,7 @@ it will be possible to move to JSF in the future.</p>
 <p>Eclipse is about "extensible platforms and exemplary tools" - and
 we take that seriously. The widget library can be extended with more widgets,
 much the same way as SWT requires knowledge of the "native" (meaning
-AJAX) technologies. As the Web Workbench will be built on the plug-in model, it
+Ajax) technologies. As the Web Workbench will be built on the plug-in model, it
 is extensible by design. </p>
 </div>
 
