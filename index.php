81dc648bca99bdd4ea4e5405cb886e8e6aa32b93
jkrause <jkrause>
2007-10-01 07:57:26 +0000

    link to demos on rap.eclipse.org

--- index.php
+++ index.php
@@ -37,12 +37,12 @@ $html = <<<EOHTML
 		and Noteworthy</a>). 
 		</p>
     <p>
-    <a href="http://rap.innoopract.com/rapdemo/rap" target="_blank">See a workbench demo online</a>.
+    <a href="http://rap.eclipse.org/rapdemo/maildemo" target="_blank">See the RAP (RCP) maildemo online</a>.
     </p>
     <p>
     A core component of RAP is the RAP Widget Toolkit, which offers SWT API to the largest extend possible (for the web). See a demo <a href="http://rap.innoopract.com/rapdemo/rap?w4t_startup=controls" target="_blank">RWT widget demo</a>.
     </p>
-    <p><a href="http://rap.innoopract.com/rapdemo/rap?w4t_startup=controls" target="_blank">
+    <p><a href="http://rap.eclipse.org/rapdemo/contols" target="_blank">
       <img src="resources/webworkbench.png" alt="A demo webworkbench"></a>
     </p>
     <p><img src="resources/workbench_pde.png" alt="Webworkbench plugin.xml">
