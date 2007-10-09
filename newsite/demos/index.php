7f54587a8d99ca0791551ae57482dd28863c3a31
jkrause <jkrause>
2007-10-09 12:28:46 +0000

    links updated

--- newsite/demos/index.php
+++ newsite/demos/index.php
@@ -1,10 +1,10 @@
 <?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
-	$pageTitle 		= "Rich Ajax Platform (RAP) Project";
-	$pageKeywords	= "Ajax, rap, w4t, eclipse rap";
-	$pageAuthor		= "Jochen Krause";
-		
-	# Paste your HTML content between the EOHTML markers!	
-	$html = <<<EOHTML
+$pageTitle 		= "Rich Ajax Platform (RAP) Project";
+$pageKeywords	= "Ajax, rap, w4t, eclipse rap";
+$pageAuthor		= "Jochen Krause";
+
+# Paste your HTML content between the EOHTML markers!
+$html = <<<EOHTML
 
 <div id="container">
   <div id="midcolumn">
@@ -12,17 +12,17 @@
     <p>&nbsp;</p>
     <table width="800" border="0" cellspacing="5" cellpadding="5">
       <tr>
-        <td width="352"><img src="controls.png" alt="controls" width="352" height="268" /></td>
+        <td width="352"><img src="../../images/controls.png" alt="controls" width="352" height="268" /></td>
         <td width="413" valign="top"><h2 align="left">RWT Controls Demo</h2>
           <p>RAP offers an UI concept (workbench) analogical to RCP that combines repeating complex programming tasks into predefined components.</p></td>
       </tr>
       <tr>
-        <td><img src="webworkbench.png" alt="workbench" width="352" height="324" /></td>
+        <td><img src="../../images/webworkbench.png" alt="workbench" width="352" height="324" /></td>
         <td valign="top"><h2>RAP Workbench Demo</h2>
           <p> <a href="http://rap.innoopract.com/rapdemo/rap">See a workbench demo online</a>. </p></td>
       </tr>
       <tr>
-        <td><img src="maildemo.png" alt="maildemo" width="343" height="268" /></td>
+        <td><img src="../../images/maildemo.png" alt="maildemo" width="343" height="268" /></td>
         <td valign="top"><h2>RAP (RCP) maildemo</h2>
           <p><a href="http://rap.eclipse.org/rapdemo/maildemo" target="_blank">See the RAP (RCP) maildemo online</a></p></td>
       </tr>
@@ -35,7 +35,7 @@
         <td colspan="2"><h2>Eclipse Discovery</h2></td>
       </tr>
       <tr>
-        <td width="464" valign="top"><img src="discovery.png" alt="discovery" width="464" height="400" /></td>
+        <td width="464" valign="top"><img src="../../images/discovery.png" alt="discovery" width="464" height="400" /></td>
         <td width="301" valign="top"><ul>
             <li>Choose from the entire Eclipse.org portfolio</li>
             <li>Add your favorite 3rd party plugins</li>
@@ -52,6 +52,6 @@
 EOHTML;
 
 
-	# Generate the web page
-	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+# Generate the web page
+$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
 ?>
