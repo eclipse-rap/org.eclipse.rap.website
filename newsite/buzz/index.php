31d8307ade301f7076946082b9ab3e87c18811eb
jkrause <jkrause>
2007-10-09 12:39:53 +0000

    buzz content changed

--- newsite/buzz/index.php
+++ newsite/buzz/index.php
@@ -1,10 +1,10 @@
 <?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
-	$pageTitle 		= "Rich Ajax Platform (RAP) Project";
-	$pageKeywords	= "Ajax, rap, w4t, eclipse rap";
-	$pageAuthor		= "Jochen Krause";
-		
-	# Paste your HTML content between the EOHTML markers!	
-	$html = <<<EOHTML
+$pageTitle 		= "Rich Ajax Platform (RAP) Project";
+$pageKeywords	= "Ajax, rap, w4t, eclipse rap";
+$pageAuthor		= "Jochen Krause";
+
+# Paste your HTML content between the EOHTML markers!
+$html = <<<EOHTML
 
 <div id="maincontent">
 	<div id="midcolumn">
@@ -25,7 +25,8 @@
 	            </li>
 				<li>
 		            <a href="http://jax-award.de/jax_award06/gewinner_en.php">
-		           	RAP wins european innovation award</a> JAX, 2006
+		           	RAP wins european innovation award</a> JAX, 2006</br>
+		           	<img src="../../jax.png"/> 
 	            </li>
 	            <li>
 		            <a href="http://eclipsezilla.eclipsecon.org/php/attachment.php?bugid=3833">
@@ -35,28 +36,22 @@
 		            <a href="http://eclipsenuggets.blogspot.com/2007/09/see-how-you-can-make-rcp-application.html">
 		           	See how you can make an RCP Application run in your browser</a> blog, Elias Volanakis 
 	            </li>
-            </ul> 
+	            <li>
+		            <a href="http://mea-bloga.blogspot.com/2007/09/web-development-revisited.html">
+		           	Web development revisited</a> blog, Chris Aniszczyk 
+	            </li>
+	            <li>
+		            <a href="http://eclipse-magazin.de/itr/ausgaben/psecom,id,366,nodeid,228.html">
+		           	RCP im Zeitalter vom Web 2.0 </a> eclipse Magazin, Volume 12, Benjamin Muskalla und Ralf Sternberg 
+	            </li>
+	          </ul> 
 		</div>
 	</div>
 </div>
-	
-
-
-Release Review Slides
-
-
-http://eclipsenuggets.blogspot.com/2007/09/see-how-you-can-make-rcp-application.html
-
-http://mea-bloga.blogspot.com/2007/09/web-development-revisited.html
-
-http://eclipse-magazin.de/itr/ausgaben/psecom,id,366,nodeid,228.html
-
-
-
 </div>
 
 EOHTML;
 
-	# Generate the web page
-	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+# Generate the web page
+$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
 ?>
