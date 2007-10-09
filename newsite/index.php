f43eded98ee244ada76c633c9043fdc55a0c8205
jkrause <jkrause>
2007-10-09 18:44:18 +0000

    update

--- newsite/index.php
+++ newsite/index.php
@@ -13,7 +13,7 @@ $html = <<<EOHTML
       <td valign="top"><p>The RAP project aims to enable developers to build rich, Ajax-enabled Web applications by using the Eclipse development model, plug-ins with the well known Eclipse workbench extenstion points and a widget toolkit with SWT API (plus JFace). The project is in the validation phase.</p>
     <div style="margin: 10px; padding: 5px; border:2px; border-color:#eee; border-style:solid; width: 750px;">
       <table border="0" cellpadding="5">
-          <tr height=9>
+          <!-- <tr height=9> -->
           <tr>
             <td width=20></td>
             <td><a href="/rap/newsite/community"><img border=0 src="../images/demos.png"></a></td>
