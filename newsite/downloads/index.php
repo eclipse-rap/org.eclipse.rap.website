422d6c54098465d4dd133eb001695e3dc766bb3f
rherrmann <rherrmann>
2007-10-12 22:48:23 +0000

    New download site

--- newsite/downloads/index.php
+++ newsite/downloads/index.php
@@ -12,212 +12,83 @@ $html = <<<EOHTML
 <div id="midcolumn">
 		<h1>$pageTitle</h1>
 
-    <p> 
-    Since RCP and RAP bundles within the same instance do not work, we are only 
-    able to provide target platforms for download.
-    This is because RAP now uses the same namespace as RCP.
-    </p>
-    <p>After downloading, extract the file in a convenient location, say <code>
-    c:\\target</code> and start Eclipse (if not yet running). Open <b>Window &gt; 
-    Preferences</b>, go to 
-    the <b>Plug-in Development &gt; Target Platform</b> preference page, set the
-    <b>Location</b> to the eclipse sub-directory of the extracted archive (e.g.
-    <code>c:\\target\\eclipse</code>). Finally click <b>Reload</b>. You should see
-    the just downloaded plug-ins appear in the plug-ins list.
-    </p>
-    <p>Note: if doing this with a workspace that already contains projects that
-    depend on the target, you may get compile errors. Restartiung Eclipse and/or
-    doing a clean build will help.
-    </p>
     <div class="homeitem3col">
-      <h3>Milestone Target Platforms</h3>
+      <p>
+      bla, bla
+      </p>
+      <h3>1.0 Release - Tooling Update Site</h3>
       <ul>
-        <li>RAP 1.0 RC1 for Eclipse 3.2
-          <a href="http://www.eclipse.org/downloads/download.php?file=/technology/rap/rap-1.0-RC1-incubation-target-3.2-20070928-1625.zip">
-            rap-1.0-RC1-incubation-target-3.2-20070928-1625.zip
+        <li>Eclipse 3.3
+          <br />
+          <b>http://download.eclipse.org/technology/rap/update-site/</b>
+          <br />
+          <a href="http://www.eclipse.org/downloads/download.php?file=/technology/rap/rap-sdk-1.0-R-site-3.3-200710091107.zip">
+            <img src="download.gif" title="Update Site Zip" alt="Update Site Zip"/>
+          </a>
+          rap-sdk-1.0-R-site-3.3-200710091107.zip
+          <br />
+        </li>
+      </ul>
+    </div>  
+      
+    <div class="homeitem3col">
+      <h3>1.0 Release - Target Platform</h3>
+      <p>
+      Since RCP and RAP bundles within the same instance do not work, we are only 
+      able to provide target platforms for download.
+      This is because RAP now uses the same namespace as RCP.
+      </p>
+      <p>After downloading, extract the file in a convenient location, say <code>
+      c:\\target</code> and start Eclipse (if not yet running). Open <b>Window &gt; 
+      Preferences</b>, go to 
+      the <b>Plug-in Development &gt; Target Platform</b> preference page, set the
+      <b>Location</b> to the eclipse sub-directory of the extracted archive (e.g.
+      <code>c:\\target\\eclipse</code>). Finally click <b>Reload</b>. You should see
+      the just downloaded plug-ins appear in the plug-ins list.
+      </p>
+      <p>Note: if doing this with a workspace that already contains projects that
+      depend on the target, you may get compile errors. Restartiung Eclipse and/or
+      doing a clean build will help.
+      </p>
+      <ul>
+        <li>RAP 1.0 for Eclipse 3.2
+          <a href="http://www.eclipse.org/downloads/download.php?file=/technology/rap/rap-1.0-R-target-3.2-20071008-1834.zip">
+            <img src="download.gif" />
+            rap-1.0-R-target-3.2-20071008-1834.zip
           </a>
           <br />
-          RAP 1.0 RC1 for Eclipse 3.3
-          <a href="http://www.eclipse.org/downloads/download.php?file=/technology/rap/rap-1.0-RC1-incubation-target-3.3-20070928-1625.zip">
-            rap-1.0-RC1-incubation-target-3.3-20070928-1625.zip
-          </a>
-          <br />
-          <a href="http://wiki.eclipse.org/index.php/Rap10RC1_News">
-            New and Noteworthy
+          RAP 1.0 for Eclipse 3.3
+          <a href="http://www.eclipse.org/downloads/download.php?file=/technology/rap/rap-1.0-R-target-3.3-20071008-1834.zip">
+            <img src="download.gif" />
+            rap-1.0-R-target-3.3-20071008-1834.zip
           </a>
           <br />
           See <a href="gettingstarted.php">here</a> for a guide on how to get 
           started.
-          <br />
-        </li>
-        <li>RAP 1.0M6 for Eclipse 3.2
-          <a href="http://www.eclipse.org/downloads/download.php?file=/technology/rap/rap-1.0-M6-incubation-target-3.2-20070817-1525.zip">
-            rap-1.0-M6-incubation-target-3.2-20070817-1525.zip
-          </a>
-          <br />
-          RAP 1.0M6 for Eclipse 3.3
-          <a href="http://www.eclipse.org/downloads/download.php?file=/technology/rap/rap-1.0-M6-incubation-target-3.3-20070817-1525.zip">
-            rap-1.0-M6-incubation-target-3.3-20070817-1525.zip
-          </a>
-          <br />
-          <a href="http://wiki.eclipse.org/index.php/Rap10M6_News">
-            New and Noteworthy
-          </a>
-          <br />
-          See <a href="gettingstarted.php">here</a> for a guide on how to get 
-          started.
-          <br />
-        </li>
-        <li>RAP 1.0M5 for Eclipse 3.2
-          <a href="http://www.eclipse.org/downloads/download.php?file=/technology/rap/rap-1.0-M5-incubation-target-3.2-20070713-1906.zip">
-            rap-1.0-M5-incubation-target-3.2-20070713-1906.zip
-          </a>
-          <br />
-          RAP 1.0M5 for Eclipse 3.3
-          <a href="http://www.eclipse.org/downloads/download.php?file=/technology/rap/rap-1.0-M5-incubation-target-3.3-20070713-1906.zip">
-            rap-1.0-M5-incubation-target-3.3-20070713-1906.zip
-          </a>
-          <br />
-          <a href="http://wiki.eclipse.org/index.php/Rap10M5_News">
-            New and Noteworthy
-          </a>
-          <br />
-        </li>
-        <li>RAP 1.0M4 for Eclipse 3.2
-          <a href="http://www.eclipse.org/downloads/download.php?file=/technology/rap/rap-1.0-M4-incubation-target-3.2-200706081830.zip">
-            rap-1.0-M4-incubation-target-3.2-200706081830.zip
-          </a>
-          <br />
-          RAP 1.0M4 for Eclipse 3.3
-          <a href="http://www.eclipse.org/downloads/download.php?file=/technology/rap/rap-1.0-M4-incubation-target-3.3-200706081830.zip">
-            rap-1.0-M4-incubation-target-3.3-200706081830.zip
-          </a>
-          <br />
-          <a href="http://wiki.eclipse.org/index.php/Rap10M4_News">
-            New and Noteworthy
-          </a>
-          <br />
         </li>
       </ul>
     </div>
     
-    <div class="homeitem3col">
-      <h3>Integration Build Target Platforms</h3>
-      <ul>
+   <div class="homeitem3col" style="width:95%; border:none; margin-left:0px">
+     <table>
+       <tr>
+         <td>
+           <img border=0 src="/mylyn/images/archives.gif"></a>
+         </td>
+         <td>
+           <table>
+             <tr>
+               <td>
+                 <h3><a href="/mylyn/builds">Archives and other builds</a></h3>
+               </td>
+               <td>RAP 1.0 RC1 older versions, Sandbox</td> 
+             </tr>
+           </table>
+         </td>
+       </tr>
+     </table>
+  </div>
       
-        <li>RAP 1.0 I-20070921 for Eclipse 3.2
-          <a href="http://www.eclipse.org/downloads/download.php?file=/technology/rap/rap-1.0-I-incubation-target-3.2-20070921-1419.zip">
-            rap-1.0-I-incubation-target-3.2-20070921-1419.zip
-          </a>
-          <br />
-          RAP 1.0 I-20070921 for Eclipse 3.3
-          <a href="http://www.eclipse.org/downloads/download.php?file=/technology/rap/rap-1.0-I-incubation-target-3.3-20070921-1419.zip">
-            rap-1.0-I-incubation-target-3.3-20070921-1419.zip
-          </a>
-        </li>
-
-        <li>RAP 1.0 I-20070914 for Eclipse 3.2
-          <a href="http://www.eclipse.org/downloads/download.php?file=/technology/rap/rap-1.0-I-incubation-target-3.2-20070914-1831.zip">
-            rap-1.0-I-incubation-target-3.2-20070914-1831.zip
-          </a>
-          <br />
-          RAP 1.0 I-20070914 for Eclipse 3.3
-          <a href="http://www.eclipse.org/downloads/download.php?file=/technology/rap/rap-1.0-I-incubation-target-3.3-20070914-1831.zip">
-            rap-1.0-I-incubation-target-3.3-20070914-1831.zip
-          </a>
-        </li>
-        <li>RAP 1.0 I-20070907 for Eclipse 3.2
-          <a href="http://www.eclipse.org/downloads/download.php?file=/technology/rap/rap-1.0-I-incubation-target-3.2-20070907-1456.zip">
-            rap-1.0-I-incubation-target-3.2-20070907-1456.zip
-          </a>
-          <br />
-          RAP 1.0 I-20070907 for Eclipse 3.3
-          <a href="http://www.eclipse.org/downloads/download.php?file=/technology/rap/rap-1.0-I-incubation-target-3.3-20070907-1456.zip">
-            rap-1.0-I-incubation-target-3.3-20070907-1456.zip
-          </a>
-        </li>
-        <li>RAP 1.0 I-20070831 for Eclipse 3.2
-          <a href="http://www.eclipse.org/downloads/download.php?file=/technology/rap/rap-1.0-I-incubation-target-3.2-20070831-1725.zip">
-            rap-1.0-I-incubation-target-3.2-20070831-1725.zip
-          </a>
-          <br />
-          RAP 1.0 I-20070831 for Eclipse 3.3
-          <a href="http://www.eclipse.org/downloads/download.php?file=/technology/rap/rap-1.0-I-incubation-target-3.3-20070831-1725.zip">
-            rap-1.0-I-incubation-target-3.3-20070831-1725.zip
-          </a>
-        </li>
-        <li>RAP 1.0 I-20070824 for Eclipse 3.2
-          <a href="http://www.eclipse.org/downloads/download.php?file=/technology/rap/rap-1.0-I-incubation-target-3.2-20070824-1323.zip">
-            rap-1.0-I-incubation-target-3.2-20070824-1323.zip
-          </a>
-          <br />
-          RAP 1.0 I-20070824 for Eclipse 3.3
-          <a href="http://www.eclipse.org/downloads/download.php?file=/technology/rap/rap-1.0-I-incubation-target-3.3-20070824-1323.zip">
-            rap-1.0-I-incubation-target-3.3-20070824-1323.zip
-          </a>
-        </li>
-        <li>RAP 1.0 I-20070810 for Eclipse 3.2
-          <a href="http://www.eclipse.org/downloads/download.php?file=/technology/rap/rap-1.0-I-incubation-target-3.2-20070810-1109.zip">
-            rap-1.0-I-incubation-target-3.2-20070810-1109.zip
-          </a>
-          <br />
-          RAP 1.0 I-20070810 for Eclipse 3.3
-          <a href="http://www.eclipse.org/downloads/download.php?file=/technology/rap/rap-1.0-I-incubation-target-3.3-20070810-1109.zip">
-            rap-1.0-I-incubation-target-3.3-20070810-1109.zip
-          </a>
-        </li>
-        <li>RAP 1.0 I-20070803 for Eclipse 3.2
-          <a href="http://www.eclipse.org/downloads/download.php?file=/technology/rap/rap-1.0-I-incubation-target-3.2-20070803-1640.zip">
-            rap-1.0-I-incubation-target-3.2-20070803-1640.zip
-          </a>
-          <br />
-          RAP 1.0 I-20070803 for Eclipse 3.3
-          <a href="http://www.eclipse.org/downloads/download.php?file=/technology/rap/rap-1.0-I-incubation-target-3.3-20070803-1640.zip">
-            rap-1.0-I-incubation-target-3.3-20070803-1640.zip
-          </a>
-        </li>
-        <li>RAP 1.0 I-20070727 for Eclipse 3.2
-          <a href="http://www.eclipse.org/downloads/download.php?file=/technology/rap/rap-1.0-I-incubation-target-3.2-20070727-1457.zip">
-            rap-1.0-I-incubation-target-3.2-20070727-1457.zip
-          </a>
-          <br />
-          RAP 1.0 I-20070727 for Eclipse 3.3
-          <a href="http://www.eclipse.org/downloads/download.php?file=/technology/rap/rap-1.0-I-incubation-target-3.3-20070727-1457.zip">
-            rap-1.0-I-incubation-target-3.3-20070727-1457.zip
-          </a>
-        </li>
-        <li>RAP 1.0 I-20070720 for Eclipse 3.2
-          <a href="http://www.eclipse.org/downloads/download.php?file=/technology/rap/rap-1.0-I-incubation-target-3.2-20070720-1623.zip">
-            rap-1.0-I-incubation-target-3.2-20070720-1623.zip
-          </a>
-          <br />
-          RAP 1.0 I-20070720 for Eclipse 3.3
-          <a href="http://www.eclipse.org/downloads/download.php?file=/technology/rap/rap-1.0-I-incubation-target-3.3-20070720-1623.zip">
-            rap-1.0-I-incubation-target-3.3-20070720-1623.zip
-          </a>
-        </li>
-      </ul>
-    </div>
-
-    <div class="homeitem3col">
-			<h3>Milestone Update Sites (only available for M1 - M3)</h3>
-			<ul>
-				<li>RAP 1.0M3 for Eclipse 3.2 / 3.3: <b>http://download.eclipse.org/technology/rap/update-site/</b>
-          <a href="http://www.eclipse.org/downloads/download.php?file=/technology/rap/rap_site_1.0.0.M3.zip">&nbsp;[zipped update site]</a>
-          <br />
-          <a href="http://wiki.eclipse.org/index.php/Rap10M3_News">New and Noteworthy</a>
-	      </li>
-				<li>RAP 1.0M2 for Eclipse 3.2 / 3.3:
-       		<a href="http://www.eclipse.org/downloads/download.php?file=/technology/rap/rap_site_1.0.0.M2.zip">&nbsp;[zipped update site]</a>
-          <br />
-          <a href="http://wiki.eclipse.org/index.php/Rap10M2_News">New and Noteworthy</a>
-	      </li>
-				<li>RAP 1.0M1 for Eclipse 3.2 / 3.3:
-          <a href="http://www.eclipse.org/downloads/download.php?file=/technology/rap/rap_site_1.0.0.M1.zip">&nbsp;[zipped update site]</a>
-	      </li>
-			</ul>
-		</div>
-
 </div>
 
 EOHTML;
