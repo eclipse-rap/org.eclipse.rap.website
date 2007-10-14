e3071f184bf98208afbe9fc0e2a021702c205938
jkrause <jkrause>
2007-10-14 13:02:24 +0000

    layout

--- newsite/index.php
+++ newsite/index.php
@@ -48,8 +48,8 @@ $html = <<<EOHTML
   </table>
   </div>
 
- <div id="rightcolumn" style="width: 160px">
-  <div class="sideitem">
+ <div id="rightcolumn">
+  <div class="sideitem" style="width: 160px">
   <h6>Active Contributors</h6>
    <ul>
     <li>
@@ -64,7 +64,7 @@ $html = <<<EOHTML
    </ul>
   </div>
 
-  <div class="sideitem">
+  <div class="sideitem" style="width: 160px">
    <h6>Meet the RAP team</h6>
    <ul>
     <li>
