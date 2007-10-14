36861a39147582ca073edea12adac2dfd090f2df
jkrause <jkrause>
2007-10-14 23:41:07 +0000

    a few new paragraphs

--- about.php
+++ about.php
@@ -10,9 +10,6 @@
 	$html = <<<EOHTML
 
 <div id="maincontent">
-<table>
-<img src="images/rap_logo.png"></img>
-</table>
 	<div id="midcolumn">
 		<div class="homeitem3col">
 			<h3>What is RAP?</h3>
@@ -23,8 +20,11 @@
 					known Eclipse workbench extension points and a widget toolkit with SWT API (plus JFace).
 				</li>
 				<li>
-					RC1 has been released
-					We have released the Release Candidate 1 build of RAP
+					Version 1.0 has been released
+					The RAP project has graduated from incubation and released version 1.0 of RAP. 
+					You can download the RAP tools / runtime from the download section on the left.
+					A user guide is part of the RAP tools and can also be accessed online from the 
+					infocenter link.
 				</li>
 			</ul>
 		</div>
@@ -32,13 +32,21 @@
 		<div class="homeitem3col">
 			<h3>RAP architecture</h3>
 			<img src="images/about/architecture.png" alt="architecture" />
-			<ul>
-				<li>License:</li>
-			</ul>
-			<ul>
-				<li>Project IP log</li>
-			</ul>
+			<p>RAP is very similar to Eclipse RCP, but instead of being executed on a desktop compunter
+			RAP is run on a server and clients can access the application with standard browsers.
+			This is mainly achieved by providing a special implementation of SWT (a subset of SWT API).
+			</p>
 		</div>
+
+		<div class="homeitem3col">
+			<h3>Tooling for RAP</h3>
+			<img src="images/tooling.png" alt="tooling" />
+			<p>RAP takes advantage of the award winning Java Development tools and the Plugin Development
+			tools provided by Eclipse.org. As applications are developed as bundles (plug-ins) and 
+			entirely in Java everything from development to launching, debugging and exporting to 
+			standard .war files works right out of the Eclipse IDE. 
+		</div>
+
 	</div> 
 </div>
 
