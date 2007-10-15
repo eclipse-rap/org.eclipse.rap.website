d86a6adeff257943d5bf437b08af019c077f3116
jkrause <jkrause>
2007-10-15 06:57:18 +0000

    new landing page and navigation bar

--- index.php
+++ index.php
@@ -1,130 +1,104 @@
 <?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
 
-	#*****************************************************************************
-	# Initial revision: Jochen Krause
-	#****************************************************************************
-
-	#
-	# Begin: page-specific settings.  Change these. 
-$pageTitle 		= "Rich Ajax Platform (RAP) Project";
-$pageKeywords	= "Ajax, rap, w4t, eclipse rap";
+# Begin: page-specific settings.  Change these.
+$pageTitle 		= "Rich Ajax Platform (RAP)";
+$pageKeywords	= "Ajax, rap, osgi, equinox, eclipse rap, equinox rap";
 $pageAuthor		= "Jochen Krause";
 
-	# Add page-specific Nav bars here
-	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
-	# $Nav->addNavSeparator("My Page Links", 	"downloads.php");
-	# $Nav->addCustomNav("My Link", "mypage.php", "_self", 3);
-	# $Nav->addCustomNav("Google", "http://www.google.com/", "_blank", 3);
-
-	# End: page-specific settings
-	#
-
-	# Paste your HTML content between the EOHTML markers!	
 $html = <<<EOHTML
+<div id="maincontent">
+  <div id="midcolumn">
 
-	<div id="midcolumn">
-		<h1>$pageTitle</h1>
-		<h2>What is RAP?</h2>
-		<p> The RAP project aims to enable developers to build rich, Ajax-enabled Web applications 
-        by using the Eclipse development model, plug-ins with the well known Eclipse workbench
-        extenstion points and a widget toolkit with SWT API (plus JFace).
-        </p>
-	    Check our <a href="http://eclipse.org/rap/noteworthy/index.php">New 
-		and Noteworthy for 1.0</a> and get a <a href="http://eclipse.org/rap/downloads.php">download</a> 
-		</p>
-    <p>
-    Try RAP online: <a href="http://rap.eclipse.org/rapdemo/rms" target="_blank">a sample enterprise application</a>.
-    </p>
-    <p>
-    A core component of RAP is the RAP Widget Toolkit, which offers SWT API to the largest extend possible (for the web). See a <a href="http://rap.eclipse.org/controlsdemo/controls" target="_blank">RWT widget demo</a>.
-    Finally this is a demo of the famous RCP mail template <a href="http://rap.eclipse.org/maildemo/rap?startup=maildemo" target="_blank">running as a RAP application</a>.
-    </p>
+  <table width="100%" border="0">
+      <tr>
+    <td colspan="2" align="center"><img src="../images/screens.png" alt="RAP" width="600" height="295" border="0"/></td>
+      <td width="15%" rowspan="2" valign="top">
+      </td>
+    </tr>
+    <tr>
+      <td colspan="3"/>&nbsp;</td>
+    </tr>
+      <tr>
+        <td width="30%" valign="top"><table border="0">
+          <tr>
+            <td width="20"></td>
+            <td><a href="/rap/newsite/community"><img border="0" src="images/community.jpg"/></a></td>
+            <td><table border="0">
+                <tr>
+                  <td><font size="+1"><a href="/rap/newsite/demos">Demos</a></font></td>
+                </tr>
+                <tr>
+                  <td>See some demos here</td>
+                </tr>
+              </table></td>
+          </tr>
+          <tr>
+            <td width="20"></td>
+            <td><a href="/rap/newsite/downloads"><img border="0" src="images/downloads.gif"/></a></td>
+            <td><table border="0">
+                <tr>
+                  <td><font size="+1"><a href="/rap/newsite/downloads">Downloads</a></font></td>
+                </tr>
+                <tr>
+                  <td>Get the latest RAP release</td>
+                </tr>
+              </table></td>
+          </tr>
+        </table></td>
+        <td width="55%" valign="top">The RAP project enables developers to build rich, Ajax-enabled Web applications by using the Eclipse development model, plug-ins with the well known Eclipse workbench extenstion points and a widget toolkit with SWT API (plus JFace). The project has graduated from incubation and released its 1.0 release.</td>
+      </tr>
+  </table>
+  </div>
 
-    <a href="http://rap.eclipse.org/maildemo/rap?startup=maildemo" target="_blank">See the RAP (RCP) maildemo online</a>.
+ <div id="rightcolumn">
+  <div class="sideitem">
+  <h6>Active Contributors</h6>
+   <ul>
+    <li>
+      <a href="http://innoopract.com" target="_blank">Innoopract</a>
+    </li>
+    <li>
+      <a href="http://oneandone.com" target="_blank">1&1</a>
+    </li>
+    <li>
+      <a href="http://www.cas.de/English/Home.asp" target="_blank">CAS</a>
+    </li>
+   </ul>
+  </div>
 
-    <p><a href="http://rap.eclipse.org/rapdemo/rms" target="_blank">
-      <img src="resources/webworkbench.png" alt="A demo webworkbench"></a>
-    </p>
-    <p><img src="resources/workbench_pde.png" alt="Webworkbench plugin.xml">
-    <br>
-    <p><img src="resources/viewpart.png" alt="A ViewPart implementation">    
-    <ul>
-      <li><a href="http://eclipse.org/rap/downloads.php">Download the latest milestone build</a></li>
-    </ul>
-
-
-    </p>
-    </p>
-			<h2>Project Scope</h2>
-			<p>The objectives of the RAP project are:</p>
-			
-			  <p><b>Enable the development of Rich Internet Applications that are based on
-			    the Eclipse Plugin architecture.</b> The Eclipse OSGi framework (Equinox)
-			    can run inside of a Web application. This has been demonstrated by several
-			    parties, and a subproject of the Equinox project has already been
-			    established (see <a href="http://www.eclipse.org/equinox/incubator/server/" target="_blank">http://www.eclipse.org/equinox/incubator/server/</a>)</p>
-			  <p><b>Enable Ajax UI development based on a Java component library with SWT api. </b>For
-			    enabling UI development based on a Java component library the project has
-			    received an initial code contribution from Innoopract (W4Toolkit). <b>NEW:</b> Based on the
-			    infrastructure provided by this code contributions we have implemented a first
-			    version of a new widget toolkit with SWT api, called RWT. More info on RWT can be
-			    found here: <a href="http://wiki.eclipse.org/index.php/RWTOverview" target="_blank">
-			    http://wiki.eclipse.org/index.php/RWTOverview</a>. RWT is using a sophisiticated JavaScript
-			    framework for client side rendering: <a href="http://qooxdoo.org" target="_blank">qooxdoo</a>.</p>
-			  <p><b>Provide a Web Workbench similar to the Eclipse platform workbench: </b>
-			    <ul>
-			      <li>provides selection service (with session scope),</li>
-			      <li>provides extension points for action sets, workbench parts,
-			        perspectives, preference pages, etc.,</li>
-			      <li>enables plug-ins to contribute to workbench parts provided by other
-			        plug-ins (e.g. action contributions)</li>
-			    </ul>
-			  </p>
-			  <p>The RAP API will be aligned with the Eclipse platform API as much as possible</p>
-
-
-		<hr class="clearer" />
-	</div>
-	<div id="rightcolumn">
-    
-      <!--
-      <div class="sideitem">
-        <img src="http://www.eclipse.org/images/gazoo-incubation.jpg" alt="Project is in incubation">
-      </div>
-      <div class="sideitem">
-        <h6>Incubation</h6><p>
-        <div align="center">
-          <a href="/projects/what-is-incubation.php">
-            <img align="center" src="/images/egg-incubation.png" border="0"/>
-          </a>
-        </div>
-        <div align="center">  
-          This project is in the 
-          <a href="/projects/dev_process/validation-phase.php">Incubation Phase</a>
-        </div>
-      </div>		
-	  -->
-		<div class="sideitem">
-			<h6>RAP in the news</h6>
-			<ul>
-				<li><a href="http://jax-award.de/jax_award06/gewinner_en.php" target="_blank">RAP wins european innovation award</a></li>
-				<li><a href="http://www.eweek.com/article2/0,1895,1939964,00.asp" target="_blank">eweek - new Ajax project for eclipse</a></li>
-			</ul>
-		</div>
-		<div class="sideitem">
-			<h6>Meet the RAP team</h6>
-			<ul>
-        <li><a href="http://www.eclipsecon.org/summiteurope2007/index.php?page=detail/&id=29" target="_blank">Eclipse Summit Europe, Ludwigsburg, October 10-11</a><span><br>RCP goes Web 2.0 - The Rich Ajax Platform (RAP)</span></li>				
-        <li><a href="http://www.eclipsecon.org/2007/index.php?page=sub/&area=directors-choice" target="_blank">EclipseCon 2007, Santa Clara, March 5-8</a><span><br>RAP - Eclipse style development for the web</span></li>
-				<li><a href="http://eclipseforumeurope.com/konferenzen/efe07/overview_sessions_eng.php?track=3#session-ewd1" target="_blank">Eclipse Forum Europe, Wiesbaden, Germany, April 23-27</a><span><br>Building Ajax applications with the Eclipse Rich Ajax Platform</span></li>
-			</ul>
-		</div>
-
-	</div>
+  <div class="sideitem">
+   <h6>Meet the RAP team</h6>
+   <ul>
+    <li>
+      <a href="http://jax.de/konferenzen/wjax07/sessions.php?language=de&tid=637" target="_blank">
+      W-JAX, Munich, November 5-9 (German)</a>
+      <span><br>
+      RCP goes Web 2.0</span>
+    </li>
+    <li>
+      <a href="http://www.eclipsecon.org/summiteurope2007/index.php?page=detail/&id=29" target="_blank">
+      Eclipse Summit Europe, Ludwigsburg, October 10-11</a>
+      <span><br>
+      RCP goes Web 2.0 - The Rich Ajax Platform (RAP)</span>
+    </li>
+    <li>
+      <a href="http://www.eclipsecon.org/2007/index.php?page=sub/&area=directors-choice" target="_blank">
+      EclipseCon 2007, Santa Clara, March 5-8</a>
+      <span><br>
+      RAP - Eclipse style development for the web</span>
+    </li>
+    <li>
+      <a href="http://eclipseforumeurope.com/konferenzen/efe07/overview_sessions_eng.php?track=3#session-ewd1" target="_blank">
+      Eclipse Forum Europe, Wiesbaden, Germany, April 23-27</a>
+      <span><br />
+      Building Ajax applications with the Eclipse Rich Ajax Platform</span>
+    </li>
+  </div>
+ </div>
+</div>
 
 EOHTML;
 
-
-	# Generate the web page
+# Generate the web page
 $App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
 ?>
