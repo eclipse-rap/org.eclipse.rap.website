3241ea368a102d2d68d5bd1445d20fe339421bbe
rherrmann <rherrmann>
2007-10-15 14:09:53 +0000

    Layout adjustments

--- newsgroup.php
+++ newsgroup.php
@@ -54,22 +54,22 @@ $html = <<<EOHTML
 
   <div class="homeitem3col">
     <h3>Useful links</h3>
-    <h4>Newsgroup Search</h3>
-      <a href="http://www.eclipse.org/search/search.cgi?cmd=Search%21&form=extended&wf=574a74&ps=10&m=all&t=5&ul=%2Fnewslists%2Fnews.eclipse.technology.rap&wm=wrd&t=News&t=Mail">
-        eclipse.technology.rap
-      </a>
-    <h4>Newsgroup web user interface (forum like)</h4>
-      <a href="http://www.eclipse.org/newsportal/thread.php?group=eclipse.technology.rap">
-        eclipse.technology.rap
-      </a>  
-    <h4>Newsgroup Browse</h4>
-      <a href="http://dev.eclipse.org/newslists/news.eclipse.technology.rap/maillist.html">
-        eclipse.technology.rap
-      </a>  
-    <h4>NNTP (news://)</h4>
-      <a href="news://news.eclipse.org/eclipse.technology.rap">
-        eclipse.technology.rap
-      </a>
+    <strong>Newsgroup Search</strong>
+    <a href="http://www.eclipse.org/search/search.cgi?cmd=Search%21&form=extended&wf=574a74&ps=10&m=all&t=5&ul=%2Fnewslists%2Fnews.eclipse.technology.rap&wm=wrd&t=News&t=Mail">
+      eclipse.technology.rap
+    </a>
+    <strong>Newsgroup web user interface (forum like)</strong>
+    <a href="http://www.eclipse.org/newsportal/thread.php?group=eclipse.technology.rap">
+      eclipse.technology.rap
+    </a>  
+    <strong>Newsgroup Browse</strong>
+    <a href="http://dev.eclipse.org/newslists/news.eclipse.technology.rap/maillist.html">
+      eclipse.technology.rap
+    </a>  
+    <strong>NNTP (news://)</strong>
+    <a href="news://news.eclipse.org/eclipse.technology.rap">
+      eclipse.technology.rap
+    </a>
   </div>
 
 </div>
