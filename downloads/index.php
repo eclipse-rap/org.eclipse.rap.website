53f5275efacb07ec9d850d334572cb6b766d6603
rherrmann <rherrmann>
2008-01-08 14:41:31 +0000

    Cap

--- downloads/index.php
+++ downloads/index.php
@@ -99,7 +99,7 @@ $html = <<<EOHTML
             rap-1.0.1-R-target-3.3-20071207-1851.zip  
           </a>
           for Eclipse 3.3
-          (<a href="../noteworthy/relnotes101.php">release notes</a>)
+          (<a href="../noteworthy/relnotes101.php">Release Notes</a>)
         </li>
         <li>
           <a href="http://www.eclipse.org/downloads/download.php?file=/technology/rap/rap-1.0.1-R-target-3.2-20071207-1851.zip">
