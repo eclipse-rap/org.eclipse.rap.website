02139cc0978eec6ffbf120051f1278ed1fbe494a
rherrmann <rherrmann>
2008-01-08 14:39:44 +0000

    Intro paragraph

--- noteworthy/news_11M1.php
+++ noteworthy/news_11M1.php
@@ -8,13 +8,9 @@ $html = <<<EOHTML
 <div id="maincontent">
 <div id="midcolumn">
   <h1>RAP 1.1 M1 - New and Noteworthy</h1>
-    <p>
-      Here are some of the more noteworthy things that will be available in 
-      milestone build M1 (Januar 7, 2008) and meanwhile serves to document
-      the development progress.    
-      <br />
-      All features documented here can be obtained from 
-      <a href="../cvs.php">CVS HEAD</a>.             
+    <p>Here are some of the more noteworthy things available in the milestone
+      build M1 (Januar 7, 2008) which is now available for 
+      <a href="http://www.eclipse.org/rap/downloads.php">download</a>. 
     </p>
     <ul>
     <!--
