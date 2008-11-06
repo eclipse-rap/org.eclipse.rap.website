db949437d0adbf792fa6829339c12c0a47d4ada7
rherrmann <rherrmann>
2008-11-06 09:46:09 +0000

    Changed bugzilla query url

--- noteworthy/news_12M3.php
+++ noteworthy/news_12M3.php
@@ -7,14 +7,15 @@ $pageAuthor        = "Ruediger Herrmann";
 $html = <<<EOHTML
 <div id="maincontent">
 <div id="midcolumn">
-  <h1>RAP 1.2 M2 - New and Noteworthy</h1>
+  <h1>RAP 1.2 M3 - New and Noteworthy</h1>
     <p>Here are some of the more noteworthy things that will be available in the 
-      milestone build M2 (October 8, 2008)
-      <!-- Meanwhile, all features listed here can be obtained from
+      milestone build M3 (November 19, 2008).
+      Meanwhile, all features listed here can be obtained from
       <a href="http://www.eclipse.org/rap/cvs.php">CVS HEAD</a>
-      -->
-      which is now available for 
+      <!--
+       which is now available for 
       <a href="http://www.eclipse.org/rap/downloads">download</a>.
+      -->
     </p>
     <p>
       <a href="https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced&short_desc_type=allwordssubstr&short_desc=&classification=RT&product=RAP&target_milestone=1.2+M2&long_desc_type=allwordssubstr&long_desc=&bug_file_loc_type=allwordssubstr&bug_file_loc=&status_whiteboard_type=allwordssubstr&status_whiteboard=&keywords_type=allwords&keywords=&bug_status=RESOLVED&bug_status=VERIFIED&bug_status=CLOSED&emailtype1=substring&email1=&emailtype2=substring&email2=&bugidtype=include&bug_id=&votes=&chfieldfrom=&chfieldto=Now&chfieldvalue=&cmdtype=doit&order=Reuse+same+sort+as+last+time&field0-0-0=noop&type0-0-0=noop&value0-0-0=">
@@ -160,13 +161,11 @@ $html = <<<EOHTML
     
     <hr />
 
-<!--
     <p>The above features are just the ones that are new since the last 
     milestone build. Summaries for earlier builds:</p>
     <ul>
       <li><a href="news_12M2.php">New for RAP 1.2 M2 (October 8, 2008)</a></li>
     </ul>
--->
     
     <p>&nbsp;</p>
 
