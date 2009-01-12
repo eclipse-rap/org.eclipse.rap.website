29e8bf4d30dc2fc69f9cd11265f8728acbbe428e
rherrmann <rherrmann>
2009-01-12 11:43:52 +0000

    1.2 M4 updates

--- noteworthy/news_12M5.php
+++ noteworthy/news_12M5.php
@@ -8,15 +8,18 @@ $html = <<<EOHTML
 <div id="maincontent">
 <div id="midcolumn">
   <h1>RAP 1.2 M5 - New and Noteworthy</h1>
-    <p>Here are some of the more noteworthy things that are available in the
-      milestone build M3 (February 16, 2009).
+    <p>Here are some of the more noteworthy things that will be available in the
+      milestone build M5 (February 16, 2009).
       Meanwhile, all features listed here can be obtained from
       <a href="http://www.eclipse.org/rap/cvs.php">CVS HEAD</a>
-      <!--
-      which is now available for 
-      <a href="http://www.eclipse.org/rap/downloads">download</a>.
-      -->
+    <!--
     </p>
+    <p>Here are some of the more noteworthy things that are available in this 
+      milestone build which is now available for 
+      <a href="http://www.eclipse.org/rap/downloads">download</a>.
+    </p>
+    -->
+    
     <p>
       <a href="https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced&short_desc_type=allwordssubstr&short_desc=&classification=RT&product=RAP&target_milestone=1.2+M4&long_desc_type=allwordssubstr&long_desc=&bug_file_loc_type=allwordssubstr&bug_file_loc=&status_whiteboard_type=allwordssubstr&status_whiteboard=&keywords_type=allwords&keywords=&bug_status=RESOLVED&bug_status=VERIFIED&bug_status=CLOSED&emailtype1=substring&email1=&emailtype2=substring&email2=&bugidtype=include&bug_id=&votes=&chfieldfrom=&chfieldto=Now&chfieldvalue=&cmdtype=doit&order=Reuse+same+sort+as+last+time&field0-0-0=noop&type0-0-0=noop&value0-0-0=">
       This list</a> shows all bugs that were fixed during this milestone. 
