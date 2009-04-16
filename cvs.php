0f09a04149b17761a4171776b568b9bb75ad0404
rherrmann <rherrmann>
2009-04-16 16:38:17 +0000

    Link to ViewCVS

--- cvs.php
+++ cvs.php
@@ -18,6 +18,13 @@ $html = <<<EOHTML
   <h1>CVS</h1>
 
   <div class="homeitem3col">
+    <h3>Browse CVS</h3>
+    <p style="margin:10px">
+      You can browse the sources online with 
+      <a href=""http://dev.eclipse.org/viewcvs/index.cgi/org.eclipse.rap/?root=RT_Project>
+      this web interface</a>.
+    </p>
+  
     <h3>CVS Checkout</h3>
   
     <p style="margin:10px">
