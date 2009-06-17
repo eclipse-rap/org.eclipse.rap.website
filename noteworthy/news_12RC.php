df812e39e5b9440b7c75eff5f2c2fac63d7f86aa
rherrmann <rherrmann>
2009-06-17 10:42:24 +0000

    RC5

--- noteworthy/news_12RC.php
+++ noteworthy/news_12RC.php
@@ -41,6 +41,14 @@ $html = <<<EOHTML
       <a href="http://www.eclipse.org/rap/downloads">download</a>.
     </p>
 
+    <h2>RC5</h2>
+    <p>
+      <a href="https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced&short_desc_type=allwordssubstr&short_desc=&classification=RT&product=RAP&target_milestone=1.2&long_desc_type=allwordssubstr&long_desc=&bug_file_loc_type=allwordssubstr&bug_file_loc=&status_whiteboard_type=allwordssubstr&status_whiteboard=&keywords_type=allwords&keywords=&bug_status=RESOLVED&bug_status=VERIFIED&bug_status=CLOSED&emailtype1=substring&email1=&emailtype2=substring&email2=&bugidtype=include&bug_id=&votes=&chfieldfrom=&chfieldto=Now&chfieldvalue=&cmdtype=doit&order=Reuse+same+sort+as+last+time&field0-0-0=noop&type0-0-0=noop&value0-0-0=">
+      This list</a> shows all bugs that were fixed in the RC5 build
+      which is now available for 
+      <a href="http://www.eclipse.org/rap/downloads">download</a>.
+    </p>
+
     <p>The above notes are just the ones that are new since the last 
     milestone build. Summaries for earlier builds:</p>
     <ul>
