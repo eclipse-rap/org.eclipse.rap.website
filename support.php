8faca7c4745e5ce687294cbecf2c0ffd2f4f1a19
rherrmann <rherrmann>
2009-06-23 14:47:34 +0000

    Support page

--- support.php
+++ support.php
@@ -14,9 +14,10 @@ $html = <<<EOHTML
       <h3>Community Newsgroup</h3>
       <p>The RAP Newsgroup is intended for the user community discussions on 
         RAP usage and troubleshooting. Developers contributing to RAP should
-        refer to the mailing list.
-      </p>
-      <p>Chances are your question has already been answered in the newsgroup, 
+        refer to the <a href="https://dev.eclipse.org/mailman/listinfo/rap-dev">
+        mailing list<a>.
+        <br />
+        Chances are your question has already been answered in the newsgroup, 
         and will save you time in not having to ask it again, and us in not 
         having to answer it again.
       </p> 
@@ -41,12 +42,12 @@ $html = <<<EOHTML
       Bugzilla web interface</a>. Please 
       <a href="https://bugs.eclipse.org/bugs/query.cgi?product=RAP">search for 
       duplicates first</a>. 
-      
-      For defects provide steps on how to reproduce. For enhancements outline 
-      a use case. 
       <br />
-      Further information on how to write bug reports can be found in the
-      <a href="http://wiki.eclipse.org/RAP_Bug_Reporting_Howto">RAP Bug Reporting Howto</a>.
+      For defects provide steps on how to reproduce. For enhancements outline 
+      a use case. Further information on how to write bug reports can be found 
+      in the
+      <a href="http://wiki.eclipse.org/RAP_Bug_Reporting_Howto">RAP Bug Reporting 
+      Howto</a>.
       <ul>
         <li><a href="https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced&short_desc_type=allwordssubstr&short_desc=&product=RAP&long_desc_type=allwordssubstr&long_desc=&bug_file_loc_type=allwordssubstr&bug_file_loc=&status_whiteboard_type=allwordssubstr&status_whiteboard=&keywords_type=nowords&keywords=plan&bug_status=NEW&bug_status=ASSIGNED&bug_status=REOPENED&bug_severity=blocker&bug_severity=critical&bug_severity=major&bug_severity=normal&bug_severity=minor&bug_severity=trivial&bug_severity=enhancement&emailtype1=substring&email1=&emailtype2=substring&email2=&bugidtype=include&bug_id=&votes=&chfieldfrom=&chfieldto=Now&chfieldvalue=&cmdtype=doit&order=Bug+Number&field0-0-0=noop&type0-0-0=noop&value0-0-0=">
           <img src="images/support/query.gif" />
