d76cfba7e15bff81e5249eebea157a3532d4e139
rherrmann <rherrmann>
2009-08-21 09:33:57 +0000

    Changed 1.3 M1 release date

--- noteworthy/index.php
+++ noteworthy/index.php
@@ -16,7 +16,7 @@ $html = <<<EOHTML
           Listed are the more noteworthy things that will be available in the 
           respective milestone builds towards version 1.3.
           <ul>
-            <li><a href="news_13M1.php">M1 (August 18, 2009)</a></li>
+            <li><a href="news_13M1.php">M1 (August 21<sup>st</sup>, 2009)</a></li>
           </ul>
           <p />        
           All features listed there can also be obtained from
