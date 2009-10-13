95d7d32a63b637e8c7145638f82afd88d31351ec
rherrmann <rherrmann>
2009-10-13 12:54:04 +0000

    N&N from XML

--- nn/news_13M2.php
+++ nn/news_13M2.php
@@ -1,6 +1,6 @@
 <?php                                                                                                                          require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");    require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php");     require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php");     $App     = new App();    $Nav    = new Nav();    $Menu     = new Menu();        include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
 
-require_once( "Noteworthy.class.php" );
+require_once( "rapNoteworthy.class.php" );
 
 $pageTitle         = "RAP Project - New and Noteworthy";
 $pageKeywords    = "Ajax, rap, eclipse rap";
@@ -28,7 +28,6 @@ $footer = <<<EOHTML
    </ul>
 EOHTML;
 
-error_reporting( -1 );
 # If this is a release, leave the second argument empty ('')
 $noteworthy = new RapNoteworthy( 'news_13.xml', 'M2', $header, $footer );
 $html = $noteworthy->getHtml();
