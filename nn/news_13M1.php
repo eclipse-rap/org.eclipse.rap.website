c88a6666af448126a8b87d512cd79e150deeb4f1
rherrmann <rherrmann>
2009-10-13 13:43:12 +0000

    N&N from XML

--- nn/news_13M1.php
+++ nn/news_13M1.php
@@ -8,24 +8,18 @@ $pageAuthor        = "Ruediger Herrmann";
 
 # Paste your HTML content between the EOHTML markers!
 $header = <<<EOHTML
-<p>Here are some of the more noteworthy things that are available in the 
-   milestone build M2 (October 2<sup>nd</sup>, 2009)
-   which is now available for 
-   <a href="http://www.eclipse.org/rap/downloads">download</a>.
+<p>Here are some of the more noteworthy things that are available in the
+  milestone build M1 (August 21<sup>st</sup> 2009) which is now available
+  for <a href="http://www.eclipse.org/rap/downloads">download</a>.
 </p>
 EOHTML;
 
 # Paste your HTML content between the EOHTML markers!
 $footer = <<<EOHTML
-<p>The above features are just the ones that are new since the last 
-   milestone build. Summaries for earlier builds:</p>
-   <ul>
-     <li><a href="news_13M1.php">New for RAP 1.3 M1 (August 21<sup>st</sup> 2009)</a></li>
-   </ul>
 EOHTML;
 
 # If this is a release, leave the second argument empty ('')
-$news = new Noteworthy( 'news_13.xml', 'M2', $header, $footer );
+$news = new Noteworthy( 'news_13.xml', 'M1', $header, $footer );
 $html = $news->getHtml();
 
 # Generate the web page
