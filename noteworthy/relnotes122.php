c251b8aa90a29672d91803bfbbf5231218335a69
bmuskalla <bmuskalla>
2010-01-26 10:33:12 +0000

    fixed page title of release notes

--- noteworthy/relnotes122.php
+++ noteworthy/relnotes122.php
@@ -2,7 +2,7 @@
 
 	#
 	# Begin: page-specific settings.  Change these. 
-$pageTitle 		= "RAP Project - 1.2.1 Release Notes";
+$pageTitle 		= "RAP Project - 1.2.2 Release Notes";
 $pageKeywords   = "Ajax, rap, osgi, equinox, eclipse rap, equinox rap";
 $pageAuthor		= "R�diger Herrmann";
 
