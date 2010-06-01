c0763b147afabf51d762449a45803d920b60ea46
rsternber <rsternber>
2010-06-01 19:05:52 +0000

    Publish 1.3 RC3

--- noteworthy/news_13RC.php
+++ noteworthy/news_13RC.php
@@ -29,14 +29,8 @@ $html = <<<EOHTML
     <p>
       <a href="https://bugs.eclipse.org/bugs/buglist.cgi?query_format=advanced&short_desc_type=allwordssubstr&short_desc=&classification=RT&product=RAP&target_milestone=1.3+RC3&long_desc_type=allwordssubstr&long_desc=&bug_file_loc_type=allwordssubstr&bug_file_loc=&status_whiteboard_type=allwordssubstr&status_whiteboard=&keywords_type=allwords&keywords=&bug_status=RESOLVED&bug_status=VERIFIED&bug_status=CLOSED&emailtype1=substring&email1=&emailtype2=substring&email2=&bugidtype=include&bug_id=&votes=&chfieldfrom=&chfieldto=Now&chfieldvalue=&cmdtype=doit&order=Reuse+same+sort+as+last+time&field0-0-0=noop&type0-0-0=noop&value0-0-0=">
       This list</a> shows all bugs that were fixed in the RC3 build
-      which will be available on June 4<sup>th</sup> 2010.
-      <br />
-      In the meanwhile, all features listed here can be obtained from
-      <a href="http://www.eclipse.org/rap/cvs.php">CVS HEAD</a>
-      <!--
       which is now available for
       <a href="http://www.eclipse.org/rap/downloads">download</a>.
-      -->
     </p>
 
     <h2>RC4</h2>
