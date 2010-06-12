10df7099ed00559c83db70297f18593dfdbe0b71
rsternber <rsternber>
2010-06-12 19:04:03 +0000

    Improved big header, separated button background, icon, and text

--- _projectCommon.php
+++ _projectCommon.php
@@ -1,55 +1,39 @@
 <?php
-	# Set the theme for your project's web pages.
-	# See the Committer Tools "How Do I" for list of themes
-	# https://dev.eclipse.org/committers/ 
-	$theme = "Nova";
+# Set the theme for your project's web pages.
+# See the Committer Tools "How Do I" for list of themes
+# https://dev.eclipse.org/committers/ 
 
-	$Nav->setLinkList(array());
-	$branding = <<<EOBRANDING
-<STYLE TYPE="text/css">
-  .sideitem { border-width: 1px 1px; }
-  body { font-size: small; }
-  #midcolumn { margin-top: 5px; }
-  #midcolumn li { margin: 8px; list-style-position:outside;
-    list-style-image: url('/eclipse.org-common/themes/Nova/images/separator.png');
-    padding: 5px 10px 5px 5px; }
-}
-</STYLE>
-<div id="branding">
-<center>
-  <h1 style="font-size:+2">Rich Ajax Platform (RAP)&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</h1>
-</center>
-</div>
-EOBRANDING;
-  $Menu->setProjectBranding($branding);
+  $theme = "Nova";
 
-  $Nav->addCustomNav("About This Project", "http://www.eclipse.org/projects/project_summary.php?projectid=rt.rap", "_self", 0);
+  $Nav->setLinkList( array() );
 
-  $Nav->addNavSeparator("RAP", "/rap/index.php");
-  $Nav->addCustomNav("Demos", "/rap/demos.php", "_self", 1);
-  $Nav->addCustomNav("Get Started", "/rap/gettingstarted.php",	"_self", 1);
+  $Nav->addCustomNav( "About This Project", "http://www.eclipse.org/projects/project_summary.php?projectid=rt.rap", "_self", 0 );
 
-  $Nav->addNavSeparator("Get RAP", "/rap/downloads/", "_self", 1);
-  $Nav->addCustomNav("Downloads", "/rap/downloads/", "_self", 1);
-  $Nav->addCustomNav("Source Code", "/rap/cvs.php", "_self", 1);
+  $Nav->addNavSeparator( "RAP", "/rap/index.php" );
+  $Nav->addCustomNav( "Demos", "/rap/demos.php", "_self", 1 );
+  $Nav->addCustomNav( "Get Started", "/rap/gettingstarted.php",	"_self", 1 );
 
-  $Nav->addNavSeparator("Resources", "/rap/documentation.php", "_self", 1);
-  $Nav->addCustomNav("Documentation", "/rap/documentation.php", "_self", 1);
-  $Nav->addCustomNav("New &amp; Noteworthy", "/rap/noteworthy/", "_self", 1);
-  $Nav->addCustomNav("Get Help", "/rap/support.php", "_self", 1);
-  $Nav->addCustomNav("Project Plan", "http://www.eclipse.org/projects/project-plan.php?projectid=rt.rap", "_self", 1);	
+  $Nav->addNavSeparator( "Get RAP", "/rap/downloads/", "_self", 1 );
+  $Nav->addCustomNav( "Downloads", "/rap/downloads/", "_self", 1 );
+  $Nav->addCustomNav( "Source Code", "/rap/cvs.php", "_self", 1 );
 
-  $Nav->addNavSeparator("Development", "/rap/contribute.php", "_self", 1);
-  $Nav->addCustomNav("Contribute", "/rap/contribute.php", "_self", 1);
-  $Nav->addCustomNav("Bugs", "/rap/bugs.php", "_self", 1);
-  $Nav->addCustomNav("Incubator", "/rap/incubator.php", "_self", 1);
-  
-  $Menu->setMenuItemList(array());
-  $Menu->addMenuItem("Home", "/rap", "_self");
-  $Menu->addMenuItem("Demos", "/rap/demos.php", "_self");
-  $Menu->addMenuItem("Documentation", "/rap/documentation.php", "_self");
-  $Menu->addMenuItem("Support", "/rap/support.php", "_self");
-  $Menu->addMenuItem("Wiki", "http://wiki.eclipse.org/RAP", "_self");
+  $Nav->addNavSeparator( "Resources", "/rap/documentation.php", "_self", 1 );
+  $Nav->addCustomNav( "Documentation", "/rap/documentation.php", "_self", 1 );
+  $Nav->addCustomNav( "New &amp; Noteworthy", "/rap/noteworthy/", "_self", 1 );
+  $Nav->addCustomNav( "Get Help", "/rap/support.php", "_self", 1 );
+  $Nav->addCustomNav( "Project Plan", "http://www.eclipse.org/projects/project-plan.php?projectid=rt.rap", "_self", 1 );	
+
+  $Nav->addNavSeparator( "Development", "/rap/contribute.php", "_self", 1 );
+  $Nav->addCustomNav( "Contribute", "/rap/contribute.php", "_self", 1 );
+  $Nav->addCustomNav( "Bugs", "/rap/bugs.php", "_self", 1 );
+  $Nav->addCustomNav( "Incubator", "/rap/incubator.php", "_self", 1 );
+
+  $Menu->setMenuItemList( array() );
+  $Menu->addMenuItem( "Home", "/rap", "_self" );
+  $Menu->addMenuItem( "Demos", "/rap/demos.php", "_self" );
+  $Menu->addMenuItem( "Documentation", "/rap/documentation.php", "_self" );
+  $Menu->addMenuItem( "Support", "/rap/support.php", "_self" );
+  $Menu->addMenuItem( "Wiki", "http://wiki.eclipse.org/RAP", "_self" );
 
   $App->Promotion = TRUE; # set true to enable current eclipse.org site-wide promo
 ?>
--- index.php
+++ index.php
@@ -1,21 +1,26 @@
-<?php                                                             
-require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php"); require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php");   require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php");  $App  = new App();  $Nav  = new Nav();  $Menu   = new Menu();   include($App->getProjectCommon());
-  $App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="style.css"/>');
+<?php
 
+require_once( $_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php" );
+require_once( $_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php" );
+require_once( $_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php" );
+$App = new App();
+$Nav = new Nav();
+$Menu = new Menu();
 
-#
+include( $App->getProjectCommon() );
+
 # Begin: page-specific settings.  Change these.
-$pageTitle    = "Rich Ajax Platform (RAP)";
+
+$pageTitle = "Rich Ajax Platform (RAP)";
 $pageKeywords = "Ajax, rap, osgi, equinox, eclipse rap, equinox rap";
-$pageAuthor   = "Jochen Krause";
+$pageAuthor = "Jochen Krause";
 
-$root = $_SERVER['DOCUMENT_ROOT'];
+$html = file_get_contents( '_index.html' );
 
-  
-# Paste your HTML content between the EOHTML markers!
-$html = file_get_contents('_index.html');
+$Nav = null; // delete right side navigation
 
-# Generate the web page
-$App->generatePage($theme, $Menu, null, $pageAuthor, $pageKeywords, $pageTitle, $html);
-  
+$App->AddExtraHtmlHeader( '<link rel="stylesheet" type="text/css" href="/rap/rap-big-header.css"/>' );
+
+$App->generatePage( $theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html );
+
 ?>
