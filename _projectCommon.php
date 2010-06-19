37870de4db35f187855857569119aede49549d7b
rsternber <rsternber>
2010-06-19 13:22:59 +0000

    Re-organized download page with expand areas, changed navigation to match the categories from the main page

--- _projectCommon.php
+++ _projectCommon.php
@@ -1,27 +1,25 @@
 <?php
 # Set the theme for your project's web pages.
 # See the Committer Tools "How Do I" for list of themes
-# https://dev.eclipse.org/committers/ 
+# https://dev.eclipse.org/committers/
 
   $Nav->setLinkList( array() );
 
   $Nav->addCustomNav( "About This Project", "http://www.eclipse.org/projects/project_summary.php?projectid=rt.rap", "_self", 0 );
 
-  $Nav->addCustomNav( "Demos", "/rap/demos/", "_self", 1 );
-  $Nav->addCustomNav( "Get Started", "/rap/gettingstarted.php",	"_self", 1 );
-
-  $Nav->addNavSeparator( "Get RAP", "/rap/downloads/", "_self", 1 );
-  $Nav->addCustomNav( "Downloads", "/rap/downloads/", "_self", 1 );
+  $Nav->addNavSeparator( "Downloads", "/rap/downloads/", "_self", 1 );
+  $Nav->addCustomNav( "Archive", "/rap/downloads/archive.php", "_self", 1 );
   $Nav->addCustomNav( "Source Code", "/rap/cvs.php", "_self", 1 );
 
-  $Nav->addNavSeparator( "Resources", "/rap/documentation.php", "_self", 1 );
-  $Nav->addCustomNav( "Documentation", "/rap/documentation.php", "_self", 1 );
+  $Nav->addNavSeparator( "Documentation", "/rap/documentation.php", "_self", 1 );
+  $Nav->addCustomNav( "Demos", "/rap/demos/", "_self", 1 );
   $Nav->addCustomNav( "New &amp; Noteworthy", "/rap/noteworthy/", "_self", 1 );
-  $Nav->addCustomNav( "Get Help", "/rap/support.php", "_self", 1 );
   $Nav->addCustomNav( "Project Plan", "http://www.eclipse.org/projects/project-plan.php?projectid=rt.rap", "_self", 1 );	
 
-  $Nav->addNavSeparator( "Development", "/rap/contribute.php", "_self", 1 );
-  $Nav->addCustomNav( "Contribute", "/rap/contribute.php", "_self", 1 );
+  $Nav->addNavSeparator( "Support", "/rap/support.php", "_self", 1 );
+  $Nav->addCustomNav( "Get Started", "/rap/gettingstarted.php",	"_self", 1 );
+
+  $Nav->addNavSeparator( "Contribute", "/rap/contribute.php", "_self", 1 );
   $Nav->addCustomNav( "Bugs", "/rap/bugs.php", "_self", 1 );
   $Nav->addCustomNav( "Incubator", "/rap/incubator.php", "_self", 1 );
 
@@ -33,6 +31,7 @@
   $Menu->addMenuItem( "Wiki", "http://wiki.eclipse.org/RAP", "_self" );
 
   $App->AddExtraHtmlHeader( '<link rel="stylesheet" type="text/css" href="/rap/rap-layout-fixes.css"/>' );
+  $App->AddExtraHtmlHeader( '<script type="text/javascript" src="http://code.jquery.com/jquery-1.4.2.min.js"></script>' );
 
 //  $App->Promotion = TRUE; # set true to enable current eclipse.org site-wide promo
 
@@ -42,6 +41,7 @@
   <a href="/rap/"><div id="rap-small-logo"></div></a>
 </div>
 EOHTML;
+    
     return $html;
   }
 
