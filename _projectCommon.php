a05a3c157c2e094dc02d800812de156ff887103b
hstaudacher <hstaudacher>
2010-06-22 16:12:10 +0000

    - created javascript to link the eclipse logo

--- _projectCommon.php
+++ _projectCommon.php
@@ -44,6 +44,14 @@
 <div id="rap-small-header">
   <a href="/rap/"><div id="rap-small-logo"></div></a>
 </div>
+<script type="text/javascript">
+  var logo = $( "#logo:first-child" );
+  var newLogo = '<div id="logo"><a href="http://www.eclipse.org">' 
+                + logo.html() + '</a></div>';
+  logo.replaceWith( newLogo );
+</script>
+
+
 EOHTML;
     
     return $html;
