184b368b951867d9cbce59a055862ca104fba405
rsternber <rsternber>
2010-07-07 20:17:37 +0000

    Remove xslt time measurement

--- downloads/1.3/index.php
+++ downloads/1.3/index.php
@@ -13,10 +13,7 @@ $pageTitle = "RAP - 1.3 Downloads";
 $pageKeywords = "ajax, rap, eclipse rap, 1.3, downloads";
 $pageAuthor = "Ralf Sternberg";
 
-$t0 = microtime( true );
 $html = xslt( 'builds.xml', '_index.xsl' );
-$t1 = microtime( true );
-$html .= "<!-- xslt real time: " . ( $t1 - $t0 ) . "-->";
 
 generateRapPage( $App, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html );
 
