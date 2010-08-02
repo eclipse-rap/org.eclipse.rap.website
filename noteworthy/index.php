0c6c56c4940422cc2e567bab2ace85e4bc35d054
rsternber <rsternber>
2010-08-02 14:35:31 +0000

    Side item styling

--- noteworthy/index.php
+++ noteworthy/index.php
@@ -69,22 +69,24 @@ $html = <<<EOHTML
   </div>
 
   <div id="rightcolumn">
-    <h6>RAP Archives</h6>
-    <ul>
-      <li>
-        <a href="/rap/downloads/1.3/">RAP 1.3 (2010)</a>
-      </li>
-      <li>
-        <a href="/rap/downloads/1.2/">RAP 1.2 (2009)</a>
-      </li>
-      <li>
-        <a href="/rap/downloads/1.1/">RAP 1.1 (2008)</a>
-      </li>
-      <li>
-        <a href="/rap/downloads/1.0/">RAP 1.0 (2007)</a>
-      </li>
-    </ul>
-  </div>
+    <div class="sideitem">
+      <h6>RAP Archives</h6>
+      <ul>
+        <li>
+          <a href="/rap/downloads/1.3/">RAP 1.3 (2010)</a>
+        </li>
+        <li>
+          <a href="/rap/downloads/1.2/">RAP 1.2 (2009)</a>
+        </li>
+        <li>
+          <a href="/rap/downloads/1.1/">RAP 1.1 (2008)</a>
+        </li>
+        <li>
+          <a href="/rap/downloads/1.0/">RAP 1.0 (2007)</a>
+        </li>
+      </ul>
+    </div>
+    </div>
 
 </div>
 EOHTML;
