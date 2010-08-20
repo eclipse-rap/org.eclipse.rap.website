3165d9477300b0c1e912c7dc8a45832efad60574
hstaudacher <hstaudacher>
2010-08-20 09:49:04 +0000

    Made changes for RAP 1.4 M1

--- noteworthy/index.php
+++ noteworthy/index.php
@@ -73,6 +73,9 @@ $html = <<<EOHTML
       <h6>RAP Archives</h6>
       <ul>
         <li>
+          <a href="/rap/downloads/1.4/">RAP 1.4 (2011)</a>
+        </li>
+        <li>
           <a href="/rap/downloads/1.3/">RAP 1.3 (2010)</a>
         </li>
         <li>
