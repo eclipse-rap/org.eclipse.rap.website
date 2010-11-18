4dbe80dfcd9401fef6089db388cab7afda939fac
rsternber <rsternber>
2010-11-18 09:46:13 +0000

    Added noteworthy for 1.4 M4

--- noteworthy/1.4/news_M4.php
+++ noteworthy/1.4/news_M4.php
@@ -15,12 +15,15 @@ $pageAuthor = "The RAP Team";
 
 $header = <<<EOHTML
 <p>
-  Here's a list of the most noteworthy things that are available in the
-  milestone build 1.4 M3 (November 2010) which is now available
-  for <a href="http://www.eclipse.org/rap/downloads/">download</a>.
+  Here's a list of the most noteworthy things that will be available in the
+  milestone build 1.4 M4 (December 2010).
+  <!--
+    which is now available
+    for <a href="http://www.eclipse.org/rap/downloads/">download</a>.
+  -->
 </p>
 <p>
-  <a href="https://bugs.eclipse.org/bugs/buglist.cgi?classification=RT&product=RAP&target_milestone=1.4+M3&bug_status=RESOLVED&bug_status=VERIFIED&bug_status=CLOSED&query_format=advanced">
+  <a href="https://bugs.eclipse.org/bugs/buglist.cgi?classification=RT&product=RAP&target_milestone=1.4+M4&bug_status=RESOLVED&bug_status=VERIFIED&bug_status=CLOSED&query_format=advanced">
   This list</a> shows all bugs that were fixed during this milestone.
 </p>
 EOHTML;
@@ -33,10 +36,11 @@ $footer = <<<EOHTML
 <ul>
   <li><a href="news_M1.php">New for RAP 1.4 M1</a></li>
   <li><a href="news_M2.php">New for RAP 1.4 M2</a></li>
+  <li><a href="news_M3.php">New for RAP 1.4 M3</a></li>
 </ul>
 EOHTML;
 
-$news = new Noteworthy( 'news_14.xml', 'M3', $header, $footer );
+$news = new Noteworthy( 'news_14.xml', 'M4', $header, $footer );
 $html = $news->getHtml();
 
 generateRapPage( $App, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
