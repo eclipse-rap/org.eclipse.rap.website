79b5c8e9dc81e06115c44894bb99996ae114888c
ifurnadjiev <ifurnadjiev>
2010-12-16 08:04:53 +0000

    Add M4 section.

--- noteworthy/index.php
+++ noteworthy/index.php
@@ -36,7 +36,7 @@ $html = <<<EOHTML
       <li>
         <a href="1.4/news_M3.php">RAP 1.4 M3</a>
         <p style="margin-top: 5px">
-          Listed are the noteworthy things available in the second milestone
+          Listed are the noteworthy things available in the third milestone
           build for RAP 1.4 (M3, available for
           <a href="/rap/downloads/">download</a>).
         </p>
