7aea7afe6f6e8b5f662b9231894d246c5982ec09
rsternber <rsternber>
2011-08-19 21:03:32 +0000

    Remove 1.3.2 from arrivals

--- _sidebar/arrivals.php
+++ _sidebar/arrivals.php
@@ -9,17 +9,6 @@
       <div class="terminal-row">
         <span class="terminal-field">
           <span class="terminal-overlay"></span>
-          <span class="terminal-text">RAP 1.3.2</span>
-        </span>
-        <span class="terminal-field">
-          <span class="terminal-overlay"></span>
-          <span class="terminal-text terminal-arrived">ARRIVED</span>
-        </span>
-      </div>
-
-      <div class="terminal-row">
-        <span class="terminal-field">
-          <span class="terminal-overlay"></span>
           <span class="terminal-text">RAP 1.4</span>
         </span>
         <span class="terminal-field">
