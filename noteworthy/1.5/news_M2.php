400cacebc318eac1d1b9858c3bd39aecbcd5905f
rherrmann <rherrmann>
2011-09-14 08:41:34 +0000

    1.5 M2 N&N page

--- noteworthy/1.5/news_M2.php
+++ noteworthy/1.5/news_M2.php
@@ -11,7 +11,9 @@ $header = <<<EOHTML
 <p>
   Here's a list of the most noteworthy things in the
   milestone build 1.5 M2 which is available
-  for <a href="http://www.eclipse.org/rap/downloads/">download</a>.
+  for <a href="http://www.eclipse.org/rap/downloads/">download</a> from October 30, 2011 on.
+  Before general availability of this milestone build, all changes described here can be 
+  explored with the most recent <a href="http://www.eclipse.org/rap/downloads/">nightly build</a>.
 </p>
 <p>
   <a href="https://bugs.eclipse.org/bugs/buglist.cgi?classification=RT&product=RAP&target_milestone=1.5+M2&bug_status=RESOLVED&bug_status=VERIFIED&bug_status=CLOSED&query_format=advanced">
--- noteworthy/index.php
+++ noteworthy/index.php
@@ -21,6 +21,13 @@
 
     <ul>
       <li>
+        <a href="1.5/news_M2.php">RAP 1.5 M2</a>
+        <p style="margin-top: 5px">
+          Listed are the noteworthy things available in the second milestone
+          build for RAP 1.5 (M2, October 30, 2011).
+        </p>
+      </li>
+      <li>
         <a href="1.5/news_M1.php">RAP 1.5 M1</a>
         <p style="margin-top: 5px">
           Listed are the noteworthy things available in the first milestone
