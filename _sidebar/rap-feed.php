87fe7d1db0ee88219bd76a456f2a088a3c8279c2
rsternber <rsternber>
2011-10-16 19:21:42 +0000

    Show a feed for all Planet Eclipse posts that contain the word 'RAP'

--- _sidebar/rap-feed.php
+++ _sidebar/rap-feed.php
@@ -6,7 +6,9 @@
 
   <script type="text/javascript">
     $( document ).ready( function () {
-      $( '#rapFeed' ).rssfeed( 'http://eclipsesource.com/blogs/tag/rap/feed/', {
+      // Alternative feed: http://eclipsesource.com/blogs/tag/rap/feed/
+      // See http://pipes.yahoo.com/pipes/pipe.info?_id=c85e08c37c50468c079de010e5fea95f
+      $( '#rapFeed' ).rssfeed( 'http://pipes.yahoo.com/pipes/pipe.run?_id=c85e08c37c50468c079de010e5fea95f&_render=rss', {
         limit: 4,
         header : false,
         key : "ABQIAAAADaXzprMke0eXhl5lyjwBJhQirors1Oy_y16QJctfjrSpBJX32hS_wL3O0Tk2wYierOO2_dXAoE0AMQ",
