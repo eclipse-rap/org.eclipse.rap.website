9a231182c34bc4dd6dfad7daf64e652fbffab69b
rsternber <rsternber>
2011-10-16 21:02:06 +0000

    Fixed variable name in php script

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
   include( $_SERVER['DOCUMENT_ROOT'] . "/rap/_projectCommon.php" );
 
-  $pageTitle = "Rich Ajax Platform (RAP)";
+  $title = "Rich Ajax Platform (RAP)";
   $navPosition = array( "home", "index" );
   printHeader( $title, $navPosition );
 
