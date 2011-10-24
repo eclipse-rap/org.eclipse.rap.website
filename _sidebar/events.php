b7c579b13c636de94052829e237b359eaed24d4b
rsternber <rsternber>
2011-10-24 10:04:08 +0000

    Added EclipseCon Europe to events

--- _sidebar/events.php
+++ _sidebar/events.php
@@ -4,6 +4,21 @@
 
   <ul>
     <li>
+      <a href="http://eclipsecon.org/">
+        EclipseCon Europe 2011
+        <br />
+        November 2 – 4
+      </a>
+      <span>
+        <ul>
+          <li>Nov 3, 09:30 - 10:00:<br/> Paul Petershagen, Eclipse in Telemedicine and Health Care – A Success Story with RCP and RAP</li>
+          <li>Nov 3, 14:00 - 14:30:<br/> Matthias Zimmermann, Andreas Hoegger, Eclipse Scout features RAP: The Best of both Worlds</li>
+          <li>Nov 3, 14:30 - 15:00:<br/> Frank Appel, Ralf Sternberg, Dynamic Web-Applications with OSGi and RAP</li>
+          <li>Nov 4, 13:30 - 15:00:<br/> Frank Appel, Ralf Sternberg, Lightweight, Dynamic Webapps Made Easy with RAP and OSGi (Presented by EclipseSource)</li>
+        </ul>
+      </span>
+    </li>
+    <li>
       <a href="http://wiki.eclipse.org/Eclipse_DemoCamps_November_2011/Munich">
         Eclipse DemoCamp Munich
         <br />
