03ebca2dfda5a636ff7446372b08cd658dfce883
rsternber <rsternber>
2011-11-09 14:48:46 +0000

    Sidebar shadows

--- _sidebar/events.php
+++ _sidebar/events.php
@@ -1,59 +1,59 @@
 <div class="sideitem">
 
   <h6>Upcoming Events</h6>
-
-  <ul>
-    <li>
-      <a href="http://eclipsecon.org/">
-        EclipseCon Europe 2011
-        <br />
-        November 2 – 4
-      </a>
-      <span>
-        <ul>
-          <li>Nov 3, 09:30 - 10:00:<br/> Paul Petershagen, Eclipse in Telemedicine and Health Care – A Success Story with RCP and RAP</li>
-          <li>Nov 3, 14:00 - 14:30:<br/> Matthias Zimmermann, Andreas Hoegger, Eclipse Scout features RAP: The Best of both Worlds</li>
-          <li>Nov 3, 14:30 - 15:00:<br/> Frank Appel, Ralf Sternberg, Dynamic Web-Applications with OSGi and RAP</li>
-          <li>Nov 4, 13:30 - 15:00:<br/> Frank Appel, Ralf Sternberg, Lightweight, Dynamic Webapps Made Easy with RAP and OSGi (Presented by EclipseSource)</li>
-        </ul>
-      </span>
-    </li>
-    <li>
-      <a href="http://wiki.eclipse.org/Eclipse_DemoCamps_November_2011/Munich">
-        Eclipse DemoCamp Munich
-        <br />
-        November 15
-      </a>
-      <span>
-        <ul>
-          <li>Frank Appel: Dynamic Web Applications with RAP and OSGi</li>
-        </ul>
-      </span>
-    </li>
-    <li>
-      <a href="http://wiki.eclipse.org/Eclipse_DemoCamps_November_2011/Hamburg">
-        Eclipse DemoCamp Hamburg
-        <br />
-        November 23
-      </a>
-      <span>
-        <ul>
-          <li>Markus Knauer: Dynamic Web Applications with RAP and OSGi</li>
-        </ul>
-      </span>
-    </li>
-    <li>
-      <a href="http://wiki.eclipse.org/Eclipse_DemoCamps_November_2011/Kaiserslautern">
-        Eclipse DemoCamp Kaiserslautern
-        <br />
-        November 24
-      </a>
-      <span>
-        <ul>
-          <li>Holger Staudacher: Dynamic Web Applications with RAP and OSGi</li>
-        </ul>
-      </span>
-    </li>
-  </ul>
-
+  <div class="sideitemcontent">
+    <ul>
+      <li>
+        <a href="http://eclipsecon.org/">
+          EclipseCon Europe 2011
+          <br />
+          November 2 – 4
+        </a>
+        <span>
+          <ul>
+            <li>Nov 3, 09:30 - 10:00:<br/> Paul Petershagen, Eclipse in Telemedicine and Health Care – A Success Story with RCP and RAP</li>
+            <li>Nov 3, 14:00 - 14:30:<br/> Matthias Zimmermann, Andreas Hoegger, Eclipse Scout features RAP: The Best of both Worlds</li>
+            <li>Nov 3, 14:30 - 15:00:<br/> Frank Appel, Ralf Sternberg, Dynamic Web-Applications with OSGi and RAP</li>
+            <li>Nov 4, 13:30 - 15:00:<br/> Frank Appel, Ralf Sternberg, Lightweight, Dynamic Webapps Made Easy with RAP and OSGi (Presented by EclipseSource)</li>
+          </ul>
+        </span>
+      </li>
+      <li>
+        <a href="http://wiki.eclipse.org/Eclipse_DemoCamps_November_2011/Munich">
+          Eclipse DemoCamp Munich
+          <br />
+          November 15
+        </a>
+        <span>
+          <ul>
+            <li>Frank Appel: Dynamic Web Applications with RAP and OSGi</li>
+          </ul>
+        </span>
+      </li>
+      <li>
+        <a href="http://wiki.eclipse.org/Eclipse_DemoCamps_November_2011/Hamburg">
+          Eclipse DemoCamp Hamburg
+          <br />
+          November 23
+        </a>
+        <span>
+          <ul>
+            <li>Markus Knauer: Dynamic Web Applications with RAP and OSGi</li>
+          </ul>
+        </span>
+      </li>
+      <li>
+        <a href="http://wiki.eclipse.org/Eclipse_DemoCamps_November_2011/Kaiserslautern">
+          Eclipse DemoCamp Kaiserslautern
+          <br />
+          November 24
+        </a>
+        <span>
+          <ul>
+            <li>Holger Staudacher: Dynamic Web Applications with RAP and OSGi</li>
+          </ul>
+        </span>
+      </li>
+    </ul>
+  </div>
 </div>
--- _sidebar/rap-feed.php
+++ _sidebar/rap-feed.php
@@ -7,20 +7,20 @@
 <div class="sideitem">
 
   <h6>Recent RAP Posts <a href="<?=$feedUrl?>" class="rssIcon"><img src="/rap/images/feed.png" alt="feed"></a></h6>
-
-  <div id="rapFeed"></div>
-
-  <script type="text/javascript">
-    $( document ).ready( function () {
-      $( '#rapFeed' ).rssfeed( '<?=urlencode( $feedUrl )?>', {
-        limit: 4,
-        header : false,
-        key : "ABQIAAAADaXzprMke0eXhl5lyjwBJhQirors1Oy_y16QJctfjrSpBJX32hS_wL3O0Tk2wYierOO2_dXAoE0AMQ",
-        titletag : "h5",
-        date : false,
-        content : true
+  <div class="sideitemcontent">
+    <div id="rapFeed"></div>
+  
+    <script type="text/javascript">
+      $( document ).ready( function () {
+        $( '#rapFeed' ).rssfeed( '<?=urlencode( $feedUrl )?>', {
+          limit: 4,
+          header : false,
+          key : "ABQIAAAADaXzprMke0eXhl5lyjwBJhQirors1Oy_y16QJctfjrSpBJX32hS_wL3O0Tk2wYierOO2_dXAoE0AMQ",
+          titletag : "h5",
+          date : false,
+          content : true
+        } );
       } );
-    } );
-  </script>
-
+    </script>
+  </div>
 </div>
