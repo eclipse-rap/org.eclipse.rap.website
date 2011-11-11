392b57a0828acb82ee2ae8a5bebcb833615fc8aa
rsternber <rsternber>
2011-11-11 16:32:54 +0000

    Added OSGi Community event

--- _sidebar/events.php
+++ _sidebar/events.php
@@ -4,21 +4,6 @@
   <div class="sideitemcontent">
     <ul>
       <li>
-        <a href="http://eclipsecon.org/">
-          EclipseCon Europe 2011
-          <br />
-          November 2 – 4
-        </a>
-        <span>
-          <ul>
-            <li>Nov 3, 09:30 - 10:00:<br/> Paul Petershagen, Eclipse in Telemedicine and Health Care – A Success Story with RCP and RAP</li>
-            <li>Nov 3, 14:00 - 14:30:<br/> Matthias Zimmermann, Andreas Hoegger, Eclipse Scout features RAP: The Best of both Worlds</li>
-            <li>Nov 3, 14:30 - 15:00:<br/> Frank Appel, Ralf Sternberg, Dynamic Web-Applications with OSGi and RAP</li>
-            <li>Nov 4, 13:30 - 15:00:<br/> Frank Appel, Ralf Sternberg, Lightweight, Dynamic Webapps Made Easy with RAP and OSGi (Presented by EclipseSource)</li>
-          </ul>
-        </span>
-      </li>
-      <li>
         <a href="http://wiki.eclipse.org/Eclipse_DemoCamps_November_2011/Munich">
           Eclipse DemoCamp Munich
           <br />
@@ -27,6 +12,18 @@
         <span>
           <ul>
             <li>Frank Appel: Dynamic Web Applications with RAP and OSGi</li>
+          </ul>
+        </span>
+      </li>
+      <li>
+        <a href="http://germany.osgiusers.org/Main/OSGiUFGTreffen2011">
+          OSGi UFG Community Event 2011, Cologne
+          <br />
+          November 16
+        </a>
+        <span>
+          <ul>
+            <li>Ralf Sternberg, Frank Appel: Dynamic Web Applications with RAP and OSGi</li>
           </ul>
         </span>
       </li>
