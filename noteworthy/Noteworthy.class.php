8e4734f181f6c1796f7058465564fc10a9229ac9
rsternber <rsternber>
2011-11-18 19:40:29 +0000

    Applied patch to display New and Noteworthy without tables, formatting

--- noteworthy/Noteworthy.class.php
+++ noteworthy/Noteworthy.class.php
@@ -1,13 +1,14 @@
 <?php
 /*******************************************************************************
- * Copyright (c) 2009 EclipseSource and others. All rights reserved.
- * This program and the accompanying materials are made available under the
- * terms of the Eclipse Public License v1.0 which accompanies this distribution, 
- * and is available at http://www.eclipse.org/legal/epl-v10.html
+ * Copyright (c) 2009 EclipseSource and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  ******************************************************************************/
 
 class Noteworthy {
-	
+
   private $html;
   private $noteworthy;
   private $version;
@@ -16,123 +17,71 @@ class Noteworthy {
   private $header;
   private $footer;
   private $categories = array();
-  
+
   function Noteworthy( $url, $milestone, $header, $footer ) {
-  	$this->noteworthy = simplexml_load_file( $url );
-  	$this->version = $this->noteworthy[ 'version' ];
-  	$this->milestone = $milestone;
+    $this->noteworthy = simplexml_load_file( $url );
+    $this->version = $this->noteworthy[ 'version' ];
+    $this->milestone = $milestone;
     // This is a release version, if $milestone is empty ('')
-  	if( $this->milestone == '' ) {
-  	  $this->release = true;
-  	}
-  	$this->header = $header;
-  	$this->footer = $footer;
-  	foreach( $this->noteworthy->entry as $entry ) {
-  	  if( ( $entry[ 'milestone' ] == $this->milestone ) || $this->release ) {
-  	    $this->categories[] = ( string )$entry[ 'category' ];
-  	  }
-  	}
-  	$this->categories = array_unique( $this->categories );
-  	$page_header = $this->setHeader();
-  	$page_content = $this->setContent();
-  	$page_footer = $this->setFooter();
-  	$this->html = $page_header.$page_content.$page_footer;
-  }
-  
-  function getHtml() {
-  	return $this->html;
-  }
-  
-  function setHeader() {
-  	$page_header = <<<EOHTML
-<div id="maincontent">
-<div id="midcolumn">
-<h1>RAP 
-EOHTML;
-    $page_header .= $this->version;
-    if( !$this->release ) {
-      $page_header .= " ";
-      $page_header .= $this->milestone;
+    if( $this->milestone == '' ) {
+      $this->release = true;
     }
-    $page_header .= <<<EOHTML
- - New and Noteworthy</h1>
-EOHTML;
+    $this->header = $header;
+    $this->footer = $footer;
+    foreach( $this->noteworthy->entry as $entry ) {
+      if( ( $entry[ 'milestone' ] == $this->milestone ) || $this->release ) {
+        $this->categories[] = ( string )$entry[ 'category' ];
+      }
+    }
+    $this->categories = array_unique( $this->categories );
+    $page_header = $this->setHeader();
+    $page_content = $this->setContent();
+    $page_footer = $this->setFooter();
+    $this->html = $page_header.$page_content.$page_footer;
+  }
+
+  function getHtml() {
+    return $this->html;
+  }
+
+  function setHeader() {
+    $milestone = '';
+    if( !$this->release ) {
+      $milestone = $this->milestone;
+    }
+    
+    $page_header = '<div id="maincontent">'
+                 . '<div id="midcolumn">'
+                 . '<h1>RAP ' . $this->version . ' ' . $milestone 
+                 . ' - New and Noteworthy</h1>';
+    
     $page_header .= $this->header;
-    // Hyperlinks to all categories in the milestone / release
     $page_header .= <<<EOHTML
 <p><ul>
 EOHTML;
-    foreach( $this->categories as $item ) {
-      $page_header .= <<<EOHTML
-<li><a href="#
-EOHTML;
-      $page_header .= $item;
-      $page_header .= <<<EOHTML
-">
-EOHTML;
-      $page_header .= $item;
-      $page_header .= <<<EOHTML
-</a></li>
-EOHTML;
-    }
     $page_header .= <<<EOHTML
 </ul></p>
 EOHTML;
-  	return $page_header;
+    return $page_header;
   }
   
   function setContent() {
-  	$page_content = "";
-  	foreach( $this->categories as $item ) {
-      // Category title
-  	  $page_content .= <<<EOHTML
-<a name="
-EOHTML;
-      $page_content .= $item;
-  	  $page_content .= <<<EOHTML
-"></a>
-<h2>
-EOHTML;
-      $page_content .= $item;
-  	  $page_content .= <<<EOHTML
-</h2>
-EOHTML;
-      $page_content .= <<<EOHTML
-<table>
-EOHTML;
+    $page_content = "";
+    foreach( $this->categories as $item ) {
       // Category items
-  	  foreach( $this->noteworthy->entry as $entry ) {
-  	  	if(    $entry[ 'category' ] == $item
-  	  	    && (    ( $entry[ 'milestone' ] == $this->milestone ) 
-  	  	         || $this->release ) ) 
-  	  	{
-          $page_content .= <<<EOHTML
-<tr valign="top" align="left">
-<td width="20%">
-<b>
-EOHTML;
-          $page_content .= $entry->title;
-          $page_content .= <<<EOHTML
-</b>
-</td>
-EOHTML;
-          $page_content .= <<<EOHTML
-<td width="80%">
-EOHTML;
-          $page_content .= $entry->description;
-          $page_content .= <<<EOHTML
-<td/>
-</tr>
-EOHTML;
-  	  	}
-  	  }
-  	  $page_content .= <<<EOHTML
-</table>
-EOHTML;
-  	}
-  	return $page_content;
+      foreach( $this->noteworthy->entry as $entry ) {
+        if(    $entry[ 'category' ] == $item
+            && (    ( $entry[ 'milestone' ] == $this->milestone ) 
+                 || $this->release ) ) 
+        {
+          $page_content .= "<h2>{$entry -> title}</h2>";
+          $page_content .= "<div class='noteworthy'>{$entry -> description}</div>";
+        }
+      }
+    }
+    return $page_content;
   }
-  
+
   function setFooter() {
     $page_footer = $this->footer;
     $page_footer .= <<<EOHTML
@@ -140,7 +89,7 @@ EOHTML;
 </div>
 </div>
 EOHTML;
-  	return $page_footer;
+    return $page_footer;
   }
 }
 
