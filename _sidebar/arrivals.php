9aeb572a8b06788359511181dea59266cbfc9791
rsternber <rsternber>
2012-02-03 16:39:53 +0000

    Published RAP 1.5 M5

--- _sidebar/arrivals.php
+++ _sidebar/arrivals.php
@@ -42,17 +42,6 @@
       <div class="terminal-row">
         <span class="terminal-field">
           <span class="terminal-overlay"></span>
-          <span class="terminal-text">RAP 1.5 M3</span>
-        </span>
-        <span class="terminal-field">
-          <span class="terminal-overlay"></span>
-          <span class="terminal-text terminal-arrived">ARRIVED</span>
-        </span>
-      </div>
-
-      <div class="terminal-row">
-        <span class="terminal-field">
-          <span class="terminal-overlay"></span>
           <span class="terminal-text">RAP 1.5 M4</span>
         </span>
         <span class="terminal-field">
@@ -68,7 +57,18 @@
         </span>
         <span class="terminal-field">
           <span class="terminal-overlay"></span>
-          <span class="terminal-text">FEB 03</span>
+          <span class="terminal-text terminal-arrived">ARRIVED</span>
+        </span>
+      </div>
+
+      <div class="terminal-row">
+        <span class="terminal-field">
+          <span class="terminal-overlay"></span>
+          <span class="terminal-text">RAP 1.5 M6</span>
+        </span>
+        <span class="terminal-field">
+          <span class="terminal-overlay"></span>
+          <span class="terminal-text">MARCH 23</span>
         </span>
       </div>
     </div>
