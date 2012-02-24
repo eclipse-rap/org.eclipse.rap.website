8f8a9a57cc85e39deea90ace105c9d9d264996b1
rsternber <rsternber>
2012-02-24 19:21:13 +0000

    Publish RAP 1.4.2

--- _sidebar/arrivals.php
+++ _sidebar/arrivals.php
@@ -20,22 +20,11 @@
       <div class="terminal-row">
         <span class="terminal-field">
           <span class="terminal-overlay"></span>
-          <span class="terminal-text">RAP 1.4.1</span>
-        </span>
-        <span class="terminal-field">
-          <span class="terminal-overlay"></span>
-          <span class="terminal-text terminal-arrived">ARRIVED</span>
-        </span>
-      </div>
-
-      <div class="terminal-row">
-        <span class="terminal-field">
-          <span class="terminal-overlay"></span>
           <span class="terminal-text">RAP 1.4.2</span>
         </span>
         <span class="terminal-field">
           <span class="terminal-overlay"></span>
-          <span class="terminal-text">FEB 24</span>
+          <span class="terminal-text terminal-arrived">ARRIVED</span>
         </span>
       </div>
 
