9fead01e95f9b21c31465a5669b2197981466abe
rsternber <rsternber>
2012-03-23 10:51:13 +0000

    Publish 1.5M6

--- _sidebar/arrivals.php
+++ _sidebar/arrivals.php
@@ -31,17 +31,6 @@
       <div class="terminal-row">
         <span class="terminal-field">
           <span class="terminal-overlay"></span>
-          <span class="terminal-text">RAP 1.5 M4</span>
-        </span>
-        <span class="terminal-field">
-          <span class="terminal-overlay"></span>
-          <span class="terminal-text terminal-arrived">ARRIVED</span>
-        </span>
-      </div>
-
-      <div class="terminal-row">
-        <span class="terminal-field">
-          <span class="terminal-overlay"></span>
           <span class="terminal-text">RAP 1.5 M5</span>
         </span>
         <span class="terminal-field">
@@ -57,7 +46,29 @@
         </span>
         <span class="terminal-field">
           <span class="terminal-overlay"></span>
-          <span class="terminal-text">MARCH 23</span>
+          <span class="terminal-text terminal-arrived">ARRIVED</span>
+        </span>
+      </div>
+
+      <div class="terminal-row">
+        <span class="terminal-field">
+          <span class="terminal-overlay"></span>
+          <span class="terminal-text">RAP 1.5 M7</span>
+        </span>
+        <span class="terminal-field">
+          <span class="terminal-overlay"></span>
+          <span class="terminal-text">MAY 11</span>
+        </span>
+      </div>
+
+      <div class="terminal-row">
+        <span class="terminal-field">
+          <span class="terminal-overlay"></span>
+          <span class="terminal-text">RAP 1.5</span>
+        </span>
+        <span class="terminal-field">
+          <span class="terminal-overlay"></span>
+          <span class="terminal-text">JUNE 27</span>
         </span>
       </div>
     </div>
