041ed51341f6766214ecc905d5f4b653904a0cc7
rsternber <rsternber>
2012-04-18 07:56:54 +0000

    Removes legal page from online documentation, layout improvements

--- developers-guide/NavigationView.php
+++ developers-guide/NavigationView.php
@@ -5,7 +5,7 @@ class NavigationView {
   private function __construct() {}
 
   public static function render( $filePath, $navigationType = null ) {
-    echo '<ul class="expandable">';
+    echo '<ul>';
     $xmlIterator = new SimpleXMLIterator( $filePath, null, true );
     self::createNavigationFromXml( $xmlIterator );
     echo '</ul>';
@@ -13,18 +13,17 @@ class NavigationView {
 
   private static function createNavigationFromXml( $xmlIterator ) {
     foreach( $xmlIterator as $element ) {
-      if( $element[ 'href' ] ) {
-        $url = str_replace( 'help/', '', $element[ 'href' ] );
-        $active = self::pathsIdentical( $url, $_GET[ 'topic' ] ) ? 'class="active"' : '';
-        echo '<li><a ' . $active . ' href="devguide.php?topic=' . $url .'">' . $element[ 'label' ] . '</a></li>';
-      } else if( $element[ 'label' ] != 'Reference' ) {
-        if( $element[ 'label' ] != 'Getting Started' ) {
+      if( $element[ 'label' ] != 'Legal' && $element[ 'label' ] != 'Reference' ) {
+        $hasChildren = $element -> count() > 0 ? true : false;
+        if( $hasChildren ) {
+          echo '<li class="category-group"><span>' . $element[ 'label' ] . '</span><ul>';
+          self::createNavigationFromXml( $element );
           echo '</ul></li>';
+        } else if( $element[ 'href' ] ) {
+          $url = str_replace( 'help/', '', $element[ 'href' ] );
+          $active = self::pathsIdentical( $url, $_GET[ 'topic' ] ) ? 'class="active"' : '';
+          echo '<li><a ' . $active . ' href="devguide.php?topic=' . $url .'">' . $element[ 'label' ] . '</a></li>';
         }
-        echo '<li class="category-group"><span>' . $element[ 'label' ] . '</span><ul>';
-      }
-      if( $element -> count() > 0 && $element[ 'label' ] != 'Reference' ) {
-        self::createNavigationFromXml( $element );
       }
     }
   }
