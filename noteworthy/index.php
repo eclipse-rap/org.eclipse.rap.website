d4347af4bf2381ef9b3aa8d1d7ae7214a2743348
ifurnadjiev <ifurnadjiev>
2012-05-08 11:45:41 +0000

    Update N&N main page

--- noteworthy/index.php
+++ noteworthy/index.php
@@ -21,7 +21,10 @@
 
   <ul>
     <li>
-      <a href="1.5/news_M6.php">RAP 1.5 M6 milestone build</a>, currently under development.
+      <a href="1.5/news_M7.php">RAP 1.5 M7 milestone build</a>, currently under development.
+    </li>
+    <li>
+      <a href="1.5/news_M6.php">RAP 1.5 M6 milestone build</a>, published March 23, 2012.
     </li>
     <li>
       <a href="1.5/news_M5.php">RAP 1.5 M5 milestone build</a>, published February 03, 2012.
