c7a0232492ddbc6667d996064178ca70987f3843
rsternber <rsternber>
2012-06-08 14:14:39 +0000

    Publish 1.5RC3

--- _sidebar/arrivals.php
+++ _sidebar/arrivals.php
@@ -20,17 +20,6 @@
       <div class="terminal-row">
         <span class="terminal-field">
           <span class="terminal-overlay"></span>
-          <span class="terminal-text">RAP 1.5 M7</span>
-        </span>
-        <span class="terminal-field">
-          <span class="terminal-overlay"></span>
-          <span class="terminal-text terminal-arrived">ARRIVED</span>
-        </span>
-      </div>
-
-      <div class="terminal-row">
-        <span class="terminal-field">
-          <span class="terminal-overlay"></span>
           <span class="terminal-text">RAP 1.5 RC1</span>
         </span>
         <span class="terminal-field">
@@ -57,7 +46,7 @@
         </span>
         <span class="terminal-field">
           <span class="terminal-overlay"></span>
-          <span class="terminal-text">JUNE 8</span>
+          <span class="terminal-text terminal-arrived">ARRIVED</span>
         </span>
       </div>
 
