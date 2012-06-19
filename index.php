cbca3b68842a9454a520f4cba8744d42504fd456
rsternber <rsternber>
2012-06-19 22:05:18 +0000

    Revise extended texts on landing page

--- index.php
+++ index.php
@@ -52,7 +52,7 @@
         RAP runs in all relevant web browsers, without any add-ons required.
         Many advanced features like gradients are rendered even in browsers without CSS3 support.
       </p><p>
-        With a JSON-based protocol, other clients can be connected.
+        It's built on an open protocol, so other clients can be connected.
         <!--<br/><a href="">learn more</a>-->
       </p>
     </div>
@@ -75,7 +75,7 @@
       </p>
     </div>
   </div>
-  
+
   <div class="antifloat" ></div>
 
   <div class="feature">
@@ -127,13 +127,20 @@
 <div class="popup">
   <h2>Widget Toolkit</h2>
   <p>
-    The Rap Widget Toolkit (RWT) features about 40 powerful Widgets that can be freely styled using 
-    the CSS-based 
-    <a href = "/rap/developers-guide/devguide.php?topic=rwt/theming.html">RWT Theming API</a>.
-    It also brings with it a great number of Layout Manager and UI-Events,
-    supports Drag and Drop and painting, and adds several web-specific features not found in SWT. 
-    The TableViwer and TreeViewer contributed by JFace can literally manage millions of entries 
-    through the use of virtual rendering and lazy loading.
+    With RAP, you don't create UIs with HTML and browser technologies, but with the Java API of SWT,
+    the widget toolkit used in Eclipse.
+    The RAP Widget Toolkit (RWT) provides a comprehensive set of powerful SWT widgets, also including
+    <a href="http://www.eclipse.org/articles/printable.php?file=Article-Understanding-Layouts/index.html">layout managers</a>
+    and event listeners.
+    It supports advanced features such as
+    <a href="http://rap.eclipsesource.com/rapdemo/examples#drag-and-drop">Drag and Drop</a> and
+    <a href="http://rap.eclipsesource.com/rapdemo/examples#canvas">drawing</a>,
+    and adds several web-specific features not found in SWT, like
+    <a href="http://rap.eclipsesource.com/rapdemo/examples#rich-label">Markup Support</a>.
+    With the JFace viewer framework, applications can easily provide access to millions of data
+    entries through the use of virtual rendering and lazy loading.
+    RWT can be freely styled using a CSS-based
+    <a href="/rap/developers-guide/devguide.php?topic=rwt/theming.html">Theming API</a>.
   </p>
     <ul>
       <li>
@@ -145,14 +152,13 @@
         <a href="http://www.eclipse.org/swt/docs.php">SWT Documentation</a>
       </li>
       <li>
-        If you are already familiar with SWT, read
-        <a href="/rap/developers-guide/devguide.php?topic=rwt/differences.html">
-          <q>Differences between the RAP Widget Toolkit and SWT</q>
-        </a>
+        If you are already familiar with SWT, read about
+        <a href="/rap/developers-guide/devguide.php?topic=rwt/differences.html">Differences between
+        RWT and SWT</a>
       </li>
       <li>
        Need more widgets? A list of known Add-Ons targeted specifically at RAP can be found in the 
-       <a href="http://wiki.eclipse.org/RAP/Add-Ons">RAP Wiki</a>. <!-- Nebula? --->
+       <a href="http://wiki.eclipse.org/RAP/Add-Ons">RAP Wiki</a> <!-- Nebula? --->
       </li>
       <li>
        Widget behavior can be enhanced using client-side event handlers:
@@ -166,44 +172,49 @@
 <div class="popup">
   <h2>Cross Platform</h2>
   <p>
-    The default RAP client supports:
+    The default RAP Web client supports these browsers:
   </p>
   <ul>
     <li>Internet Explorer 7+</li>
-    <li>Firefox 3.5+</li>
     <li>Google Chrome 7+</li>
+    <li>Firefox 3.5+</li>
     <li>Safari 4+</li>
     <li>Opera 10+</li>
     <li>iOs 5+</li>
     <li>Android 3 (Limited)</li>
   </ul>
   <p>
-    Internet Explorer 6 is supported until RAP 1.4. Other browser may work if based on 
-    a Trident, Gecko or Webkit engine. JavaScript needs to be enabled. Plug-Ins are not required,
-    but may be used by custom widgets. Support on mobile platforms has
+    No browser plug-ins are required by the default client, only JavaScript needs to be enabled.
+    However, custom widgets are free to build on any third-party API.
+    Other browser may work if based on a Trident, Gecko or Webkit engine.
+    Support on mobile browsers has
     <a href ="http://wiki.eclipse.org/RAP/Mobile_Browser">some limitations</a>.
   </p>
   <p>
-    Other clients than the default one are currently not part of the RAP Project itself, but 
-    possible due to the <a href="http://wiki.eclipse.org/RAP/Protocol">JSON-Protocol</a> introduced in RAP 1.5.
+    Other platforms can be supported by alternative RAP clients connecting to RAP's
+    <a href="http://wiki.eclipse.org/RAP/Protocol">open protocol</a>.
+    As an example, native clients for mobile platforms are available as a commercial add-on.
   </p>
 </div>
 
 <div class="popup">
   <h2>Integration</h2>
   <p>
+    Making it possible to integrate RAP with other Java technologies is one of our main objectives.
+    We're doing so by making RAP compatible with JEE and OSGi and by limiting
+    dependencies to the necessary minimum.
     A partial list of compatible technologies:
   </p>
   <ul>
     <li>
-      RAP applications can be deployed directly as OSGi bundles on 
-      <a href="http://wiki.eclipse.org/RAP/Running_RAP_on_virgo">Virgo</a>, 
-      <a href="http://wiki.eclipse.org/RAP/Running_RAP_on_WebSphere">WebSphere</a> 
-      and many other application server.
+      RAP applications can be deployed directly as OSGi bundles on
+      <a href="http://wiki.eclipse.org/RAP/Running_RAP_on_virgo">Virgo</a>,
+      <a href="http://wiki.eclipse.org/RAP/Running_RAP_on_WebSphere">WebSphere</a>
+      and many other application servers.
     </li>
     <li>
-      If you prefer, it can also be 
-      <a href="/rap/developers-guide/devguide.php?topic=advanced/deployment.html">deployed</a> 
+      If you prefer, it can also be
+      <a href="/rap/developers-guide/devguide.php?topic=advanced/deployment.html">deployed</a>
       as <code>.war</code> file in a traditional servlet container like Tomcat.
     </li>
     <li>
@@ -213,7 +224,7 @@
       <a href="http://wiki.eclipse.org/RAP/RWT_Cluster">clustering</a>.
     </li>
     <li>
-      <a href="http://wiki.eclipse.org/RAP/Equinox_Security_Integration">Equinox Security Integration</a> 
+      <a href="http://wiki.eclipse.org/RAP/Equinox_Security_Integration">Equinox Security Integration</a>
       ensures your data is safe at all times.
     </li>
   </ul>
@@ -222,41 +233,47 @@
 <div class="popup">
   <h2>Tool Support</h2>
   <p>
-    The <a href="http://www.eclipse.org/jdt">Eclipse Java Development Tools</a> 
-    (JDT) that come with the Eclipse IDE give you: 
+    Many of the Eclipse tools designed for Java or SWT development can be used with RAP.
   </p>
   <ul>
     <li>
-      Code highlighting and auto-completion.
+      You can use the full power of the
+      <a href="http://www.eclipse.org/jdt">Java Development Tools</a> (JDT)
+      including automated refactorings,
+      <a href="http://www.vogella.com/articles/EclipseDebugging/article.html">debugger</a>,
+      <a href="http://www.vogella.com/articles/JUnit/article.html">test runner</a>
+      etc.
     </li>
     <li>
-      JUnit, to 
-      <a href="http://www.vogella.com/articles/JUnit/article.html">write unit-tests</a> 
-      for your application.
+      The Plug-in Development Environment (PDE) assists with the creation of OSGi bundles,
+      dependency management, maintaining a target platform, etc.
     </li>
     <li>
-      A powerful <a href="http://www.vogella.com/articles/EclipseDebugging/article.html">debugger</a>.
+      The <a href="http://www.eclipse.org/windowbuilder/">Eclipse WindowBuilder</a> provides an
+      excellent UI designer for SWT.
     </li>
     <li>
-      And
-      <a href="http://www.eclipse.org/jdt/core/index.php">more...</a>
+      WAR files can be created using the WAR product tools from the Libra project.
+    </li>
+    <li>
+      Many more great add-ons are available for Java development in Eclipse such as the
+      <a href="http://www.eclemma.org/">EclEmma code coverage integration</a> or
+      <a href="http://findbugs.sourceforge.net/downloads.html">Findbugs for static code analysis</a>....
     </li>
   </ul>
-  <p>
-    Many other tools designed for Java or SWT can be used with RAP.
-    For example, you can use <a href="http://www.eclipse.org/windowbuilder/">Eclipse WindowBuilder</a>
-    (<a href="http://www.eclipse.org/windowbuilder/download.php">installed separately</a>) 
-    to design your UI, or Apache <a href="http://jmeter.apache.org/">JMeter</a>
-    for 
-    <a href="http://wiki.eclipse.org/RAP/LoadTesting">load testing</a>.
-  </p>
 </div>
 
 <div class="popup">
   <h2>Single Sourcing</h2>
   <p>
-    Due to RAPs high compatibility to SWT, JFace, Workbench and many Java libraries, 
-    a lot of code targeted at the desktop can now be used for your web application. 
+    RAP allows you to address different platforms with a shared code base.
+    Applications can be developed for the desktop, the web browser, and even mobile clients without
+    duplicating code.
+    Due to RAP's high compatibility with the Eclipse UI technologies, a lot of existing code
+    targeted at the desktop can be re-used for your web application with minimal changes.
+    And besides the code, you can also re-use your developer tools and skills.
+  </p>
+  <p>
     Typical scenarios are:
   </p>
   <ul>
@@ -272,15 +289,15 @@
     <li>
       Utilize RCP-compatible open source libraries and frameworks. Examples:
       <ul>
-        <li> 
-          The <a href="http://eclipse.org/modeling/emf/">Eclipse Modeling Framework</a> 
-        can
-        <a href="http://wiki.eclipse.org/RAP/EMF_Integration">run on RAP</a>.
+        <li>
+          The <a href="http://eclipse.org/modeling/emf/">Eclipse Modeling Framework</a>
+          can
+          <a href="http://wiki.eclipse.org/RAP/EMF_Integration">create RAP UIs</a>.
         </li>
         <li>
-          Use <a href="http://eclipse.org/birt">Eclipse Birt</a> (with 
-          <a href="http://wiki.eclipse.org/RAP/BIRT_Integration">some adjustments</a>
-          ) to create charts in RAP.
+          Use <a href="http://eclipse.org/birt">Eclipse BIRT</a> (with
+          <a href="http://wiki.eclipse.org/RAP/BIRT_Integration">some adjustments</a>)
+          to create charts in RAP.
         </li>
         <li>
           <a href="/rap/users/#riena">Eclipse Riena</a> 
@@ -297,25 +314,24 @@
 <div class="popup">
   <h2>Open Source</h2>
   <p>
-    All code can be freely used 
-    <a href="/rap/users/">commercially or in open source projects</a>. It is recommended to 
-    use the latest <a href="http://www.eclipse.org/downloads/">major release</a> 
-    or <a href="http://www.eclipse.org/downloads/index-developer.php">milestone</a>, but if you
-    want to be on the beeding edge, you can also use a <a href="/rap/downloads/">nightly build</a>
-    or even check out the source code directly from the <a href="/rap/source/">git repository</a>.
+    All code can be freely used in open source projects and in
+    <a href="/rap/users/">commercial applications</a>.
+    On the <a href="/rap/downloads/">downloads page</a>, you always get the latest versions.
+    We also offer public nightly builds to make all changes available immediately.
+    You can check out the source code directly from the <a href="/rap/source/">git repository</a>
+    or even fork us on <a href="https://github.com/eclipse/rap">github</a>.
   </p>
   <p>
-    If you require
-    help beyond the <a href="/rap/developers-guide/">RAP Developer's Guide</a>, there is an active
-    community maintaining a 
+    If you require help beyond the <a href="/rap/developers-guide/">RAP Developer's Guide</a>,
+    there is an active community maintaining a
     <a href="http://www.eclipse.org/forums/index.php?t=thread&frm_id=34">Newsgroup</a>,
-    a <a href="http://wiki.eclipse.org/RAP">Wiki</a> and the <a href="/rap/incubator/">Icubator project</a>.
+    a <a href="http://wiki.eclipse.org/RAP">Wiki</a> and the <a href="/rap/incubator/">Incubator project</a>.
     Bugs can be reported in the <a href="/rap/bugs/">RAP Bugzilla</a> and will be  processed 
-    transparently. Professional services are also <a href ="/rap/support/professional-services/">available</a>.
+    transparently.
+    Professional services are also <a href ="/rap/support/professional-services/">available</a>.
   </p>
 </div>
 <?php
   includeSidebar( "rap-feed-for-home.php" );
   printFooter();
 ?>
-
