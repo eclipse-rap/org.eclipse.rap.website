c7767b54e58488b0a1aa93f1f23b4add7513c220
rsternber <rsternber>
2012-06-27 11:13:41 +0000

    Fix URL in rss feeds

--- _sidebar/rap-feed-for-home.php
+++ _sidebar/rap-feed-for-home.php
@@ -2,12 +2,12 @@
   // See http://pipes.yahoo.com/pipes/pipe.info?_id=c85e08c37c50468c079de010e5fea95f
   // Alternative feed: http://eclipsesource.com/blogs/tag/rap/feed/
 
-  $feedUrl = "http://pipes.yahoo.com/pipes/pipe.run?_id=c85e08c37c50468c079de010e5fea95f&amp;_render=rss";
+  $feedUrl = "http://pipes.yahoo.com/pipes/pipe.run?_id=c85e08c37c50468c079de010e5fea95f&_render=rss";
 
 ?>
 
 <div id="blog-posts-container">
-  <a href="<?=$feedUrl?>" class="rssIcon"><img src="/rap/images/feed_dark.png" alt="feed" /></a>
+  <a href="<?=urlencode( $feedUrl )?>" class="rssIcon"><img src="/rap/images/feed_dark.png" alt="feed" /></a>
   <h2>Recent blog posts</h2>
   <div id="blog-posts"></div>
   <div class="stop"></div>
@@ -15,7 +15,7 @@
 
 <script type="text/javascript">
 
-  $( '#blog-posts' ).rssfeed( '<?=$feedUrl?>', {
+  $( '#blog-posts' ).rssfeed( '<?=urlencode( $feedUrl )?>', {
     limit: 3,
     header : false,
     key : "ABQIAAAADaXzprMke0eXhl5lyjwBJhQirors1Oy_y16QJctfjrSpBJX32hS_wL3O0Tk2wYierOO2_dXAoE0AMQ",
