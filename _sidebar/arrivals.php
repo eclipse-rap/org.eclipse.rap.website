9fc74a8b1ee4db75b03a39578deebc0ad55b1def
Ralf Sternberg <rsternberg@eclipsesource.com>
2013-01-18 11:02:18 +0100

    Publish 2.0 RC1, fix 1.5.2 date in arrivals

--- _sidebar/arrivals.php
+++ _sidebar/arrivals.php
@@ -7,28 +7,6 @@
     <div class="terminal-row">
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
-        <span class="terminal-text">RAP 1.5.1</span>
-      </span>
-      <span class="terminal-field">
-        <span class="terminal-overlay"></span>
-        <span class="terminal-text terminal-arrived">ARRIVED</span>
-      </span>
-    </div>
-
-    <div class="terminal-row">
-      <span class="terminal-field">
-        <span class="terminal-overlay"></span>
-        <span class="terminal-text">RAP 2.0 M3</span>
-      </span>
-      <span class="terminal-field">
-        <span class="terminal-overlay"></span>
-        <span class="terminal-text terminal-arrived">ARRIVED</span>
-      </span>
-    </div>
-
-    <div class="terminal-row">
-      <span class="terminal-field">
-        <span class="terminal-overlay"></span>
         <span class="terminal-text">RAP 2.0 M4</span>
       </span>
       <span class="terminal-field">
@@ -44,14 +22,14 @@
       </span>
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
-        <span class="terminal-text">JAN 18</span>
+        <span class="terminal-text terminal-arrived">ARRIVED</span>
       </span>
     </div>
 
     <div class="terminal-row">
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
-        <span class="terminal-text">RAP 1.5.2</span>
+        <span class="terminal-text">RAP 2.0 RC2</span>
       </span>
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
@@ -66,9 +44,21 @@
       </span>
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
-        <span class="terminal-text">FEB 8</span>
+        <span class="terminal-text">FEB 11</span>
       </span>
     </div>
+
+    <div class="terminal-row">
+      <span class="terminal-field">
+        <span class="terminal-overlay"></span>
+        <span class="terminal-text">RAP 1.5.2</span>
+      </span>
+      <span class="terminal-field">
+        <span class="terminal-overlay"></span>
+        <span class="terminal-text">FEB 22</span>
+      </span>
+    </div>
+
   </div>
 
 </div>
