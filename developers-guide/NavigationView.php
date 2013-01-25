3cfd5bab6a1e705a7f562da21e325c8cf61dea9c
Tim Buschtöns <tbuschto@eclipsesource.com>
2013-01-25 17:33:58 +0100

    Generate links to references

--- developers-guide/NavigationView.php
+++ developers-guide/NavigationView.php
@@ -16,34 +16,69 @@ class NavigationView {
     return $result;
   }
 
-  private static function createNavigationFromXml( $xmlIterator, $version ) {
+  private static function createNavigationFromXml( $xmlIterator, $version, $depth = 0 ) {
     $result = '';
-    $topicPath = DevGuideUtils::$versions[ $version ][ 'topicPath' ];
     foreach( $xmlIterator as $element ) {
       $label = $element[ 'label' ];
       if( !self::navEntryIsExcluded( $label ) ) {
         $hasChildren = $element -> count() > 0 ? true : false;
-        if( $hasChildren ) {
+        if( $hasChildren && $depth == 0 ) {
           $result .= '<li class="category-group"><span><span class="arrow"></span>' . $label . '</span><ul>';
-          $result .= self::createNavigationFromXml( $element, $version );
+          $result .= self::createNavigationFromXml( $element, $version, $depth + 1 );
           $result .= '</ul></li>';
-        } else if( isset( $element[ 'href' ] ) ) {
-          $url = str_replace( $topicPath, '', $element[ 'href' ] );
-          $active = '';
-          if( isset( $_GET[ 'topic' ] ) ) {
-            $active = $url === $_GET[ 'topic' ] ? 'class="active"' : '';
-          }
-          $result .=   '<li><a '
-                     . $active
-                     . ' href="devguide.php?topic='
-                     . $url
-                     . '&version='
-                     . $version
-                     . '">'
-                     . $label
-                     . '</a></li>';
+        } else {
+          $url = self::createLinkUrl( self::getHref( $element ), $version );
+          $result .= self::createLinkItem( $url, $label );
         }
       }
+    }
+    return $result;
+  }
+
+  private static function getHref( $element ) {
+    $result;
+    if( $element[ 'href' ] ) {
+      $result = $element[ 'href' ];
+    } else if( $element->link[ 0 ] ) {
+      $toc = $element->link[ 0 ][ 'toc' ];
+      $result = str_replace( 'toc.xml', 'index.html', $toc );
+    }
+    return $result;
+  }
+
+  private static function createLinkUrl( $href, $version ) {
+    $topicPath = DevGuideUtils::$versions[ $version ][ 'topicPath' ];
+    $apiUrl = DevGuideUtils::$versions[ $version ][ 'apiUrl' ];
+    $topicUrl = str_replace( $topicPath, '', $href );
+    if( self::startsWith( $topicUrl, 'reference/' ) ) {
+      $url = $apiUrl . substr( $topicUrl, 10 );
+    } else {
+      $url = 'devguide.php?topic=' . $topicUrl . '&version=' . $version;
+    }
+    return $url;
+  }
+
+  private static function createLinkItem( $url, $label ) {
+    $active = '';
+    $style = self::createLinkStyle( $url );
+    if( isset( $_GET[ 'topic' ] ) ) {
+      $active = $url === $_GET[ 'topic' ] ? ' class="active"' : '';
+    }
+    $result =   '<li><a'
+               . $active
+               . $style
+               . ' href="'
+               . $url
+               . '">'
+               . $label
+               . '</a></li>';
+    return $result;
+  }
+
+  private static function createLinkStyle( $url ) {
+    $result = '';
+    if( self::startsWith( $url, 'http' ) ) {
+      $result = ' style="padding-right:13px;background:url(../images/external.png) center right no-repeat"';
     }
     return $result;
   }
@@ -54,9 +89,6 @@ class NavigationView {
       case 'Legal':
         $result = true;
         break;
-      case 'Reference':
-        $result = true;
-        break;
       case 'Introduction':
         if( self::$introductionCount < 1 ) {
           self::$introductionCount++;
@@ -65,6 +97,10 @@ class NavigationView {
         }
     }
     return $result;
+  }
+
+  private static function startsWith( $haystack, $needle ) {
+    return !strncmp( $haystack, $needle, strlen( $needle ) );
   }
 
 }
