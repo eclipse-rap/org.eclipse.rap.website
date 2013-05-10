3c9f6fb4b0a97d6426ee9b7024733b21c672bcd6
Ralf Sternberg <rsternberg@eclipsesource.com>
2013-05-10 10:52:47 +0200

    Publish 2.1M2, revise N&N

--- _sidebar/arrivals.php
+++ _sidebar/arrivals.php
@@ -44,7 +44,7 @@
       </span>
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
-        <span class="terminal-text">MAY 10</span>
+        <span class="terminal-text terminal-arrived">ARRIVED</span>
       </span>
     </div>
 
