1cf608c770eaf4b2f5203d8d209dd3a25e77e288
Markus Knauer <mknauer@eclipsesource.com>
2013-10-04 15:47:02 +0200

    RAP 2.2 M2 released

--- _sidebar/arrivals.php
+++ _sidebar/arrivals.php
@@ -7,29 +7,7 @@
     <div class="terminal-row">
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
-        <span class="terminal-text">RAP 2.0.1</span>
-      </span>
-      <span class="terminal-field">
-        <span class="terminal-overlay"></span>
-        <span class="terminal-text terminal-arrived">ARRIVED</span>
-      </span>
-    </div>
-
-    <div class="terminal-row">
-      <span class="terminal-field">
-        <span class="terminal-overlay"></span>
         <span class="terminal-text">RAP 2.1.1</span>
-      </span>
-      <span class="terminal-field">
-        <span class="terminal-overlay"></span>
-        <span class="terminal-text terminal-arrived">ARRIVED</span>
-      </span>
-    </div>
-
-    <div class="terminal-row">
-      <span class="terminal-field">
-        <span class="terminal-overlay"></span>
-        <span class="terminal-text">RAP 2.2 M1</span>
       </span>
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
@@ -44,7 +22,18 @@
       </span>
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
-        <span class="terminal-text">OCT 3</span>
+        <span class="terminal-text terminal-arrived">ARRIVED</span>
+      </span>
+    </div>
+
+    <div class="terminal-row">
+      <span class="terminal-field">
+        <span class="terminal-overlay"></span>
+        <span class="terminal-text">RAP 2.2 M3</span>
+      </span>
+      <span class="terminal-field">
+        <span class="terminal-overlay"></span>
+        <span class="terminal-text">NOV 15</span>
       </span>
     </div>
 
