479fb0f7ab4778cec03d3177c3c786477c4b495b
Ralf Sternberg <rsternberg@eclipsesource.com>
2013-11-14 22:07:47 +0100

    Prepare RAP 2.2 M3

--- _sidebar/arrivals.php
+++ _sidebar/arrivals.php
@@ -33,7 +33,40 @@
       </span>
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
-        <span class="terminal-text">NOV 15</span>
+        <span class="terminal-text terminal-landing">LANDING</span>
+      </span>
+    </div>
+
+    <div class="terminal-row">
+      <span class="terminal-field">
+        <span class="terminal-overlay"></span>
+        <span class="terminal-text">RAP 2.2 RC1</span>
+      </span>
+      <span class="terminal-field">
+        <span class="terminal-overlay"></span>
+        <span class="terminal-text">NOV 29</span>
+      </span>
+    </div>
+
+    <div class="terminal-row">
+      <span class="terminal-field">
+        <span class="terminal-overlay"></span>
+        <span class="terminal-text">RAP 2.2 RC2</span>
+      </span>
+      <span class="terminal-field">
+        <span class="terminal-overlay"></span>
+        <span class="terminal-text">DEC 6</span>
+      </span>
+    </div>
+
+    <div class="terminal-row">
+      <span class="terminal-field">
+        <span class="terminal-overlay"></span>
+        <span class="terminal-text">RAP 2.2</span>
+      </span>
+      <span class="terminal-field">
+        <span class="terminal-overlay"></span>
+        <span class="terminal-text">DEC 20</span>
       </span>
     </div>
 
