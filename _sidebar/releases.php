9fedc0af88c1bae45c3bfebc2c03b1b6c8cf7ef2
Ralf Sternberg <rsternberg@eclipsesource.com>
2013-12-06 10:15:18 +0100

    Publish RAP 2.2 RC2, adjust and publish RAP 2.3 schedule

--- _sidebar/releases.php
+++ _sidebar/releases.php
@@ -5,6 +5,9 @@
   <div class="sideitemcontent">
     <ul>
       <li>
+        <em><a href="/rap/downloads/2.3/">RAP 2.3&emsp;June 2014 (planned)</a></em>
+      </li>
+      <li>
         <em><a href="/rap/downloads/2.2/">RAP 2.2&emsp;December 2013 (planned)</a></em>
       </li>
       <li>
--- noteworthy/index.php
+++ noteworthy/index.php
@@ -1,5 +1,5 @@
 <?php
 
-  header( "Location: /rap/noteworthy/2.2/?build=RC1" );
+  header( "Location: /rap/noteworthy/2.2/?build=RC2" );
 
 ?>
