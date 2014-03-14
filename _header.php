842d251c3b13abbee9fdf744bde420e6e7109ab4
Tim Buschtöns <tbuschto@eclipsesource.com>
2014-03-14 10:34:02 +0100

    Allow loading of external JS files independent of protocol
    Using https to visit the site may cause the browser to block scripts
    that are explicitly loaded by http.

--- _header.php
+++ _header.php
@@ -6,7 +6,7 @@
   <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
   <meta name="keywords" content="rap, rwt, osgi, eclipse, equinox, eclipse rap, ajax" />
   <meta name="google-site-verification" content="kMX1SI1MRFpyzEPh4yv_TXRUpKLmBVvENEfQtjdZuwI" />
-  <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,400italic,700' rel='stylesheet' type='text/css' />
+  <link href='//fonts.googleapis.com/css?family=Open+Sans:400,400italic,700' rel='stylesheet' type='text/css' />
   <link rel="stylesheet" type="text/css" href="/rap/_theme/reset.css" media="screen" />
   <link rel="stylesheet" type="text/css" href="/rap/_theme/layout.css" media="screen" />
 <? if( isset( $PAGE_ADD_CSS ) ) { ?>
@@ -15,8 +15,8 @@
   <link rel="stylesheet" type="text/css" href="/rap/_theme/print.css" media="print" />
   <link rel="stylesheet" type="text/css" href="/rap/_theme/prettify/prettify.css" />
   <!--[if lt IE 8]><link rel="stylesheet" type="text/css" href="/rap/rap-layout-fixes-ie.css"/><![endif]-->
-  <script type="text/javascript" src="http://code.jquery.com/jquery-1.4.2.min.js"></script>
-  <script type="text/javascript" src="http://w3.eclipsesource.com/www/js/rap/jquery.zrssfeed.min.js"></script>
+  <script type="text/javascript" src="//code.jquery.com/jquery-1.4.2.min.js"></script>
+  <script type="text/javascript" src="//w3.eclipsesource.com/www/js/rap/jquery.zrssfeed.min.js"></script>
   <script type="text/javascript" src="/rap/script.js"></script>
   <script type="text/javascript" src="/rap/_theme/prettify/prettify.js"></script>
   <script type="text/javascript" src="/rap/_theme/prettify/lang-css.js"></script>
