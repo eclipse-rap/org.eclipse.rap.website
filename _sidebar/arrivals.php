9587ea6be8505cc2de173d81b2016bda43ccbb2b
Markus Knauer <mknauer@eclipsesource.com>
2014-05-23 11:40:11 +0200

    Mark RAP 2.3RC1 as complete/arrived
    Signed-off-by: Markus Knauer <mknauer@eclipsesource.com>

--- _sidebar/arrivals.php
+++ _sidebar/arrivals.php
@@ -29,7 +29,7 @@
     <div class="terminal-row">
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
-        <span class="terminal-text">RAP 2.3 M2</span>
+        <span class="terminal-text">RAP 2.3 M3</span>
       </span>
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
@@ -40,11 +40,22 @@
     <div class="terminal-row">
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
-        <span class="terminal-text">RAP 2.3 M3</span>
+        <span class="terminal-text">RAP 2.3 RC1</span>
       </span>
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
         <span class="terminal-text terminal-arrived">ARRIVED</span>
+      </span>
+    </div>
+
+    <div class="terminal-row">
+      <span class="terminal-field">
+        <span class="terminal-overlay"></span>
+        <span class="terminal-text">RAP 2.3 RC2</span>
+      </span>
+      <span class="terminal-field">
+        <span class="terminal-overlay"></span>
+        <span class="terminal-text">MAY 30</span>
       </span>
     </div>
 
