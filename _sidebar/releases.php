be2aa28b985089d84dc11d79d1867c880e55a359
Ralf Sternberg <rsternberg@eclipsesource.com>
2014-06-25 18:17:07 +0200

    Add "Luna" to release sidebar

--- _sidebar/releases.php
+++ _sidebar/releases.php
@@ -8,7 +8,7 @@
         <em>RAP 3.0&emsp;December 2014 (planned)</em>
       </li>
       <li>
-        <a href="/rap/downloads/2.3/">RAP 2.3&emsp;June 2014</a>
+        <a href="/rap/downloads/2.3/">RAP 2.3&emsp;June 2014 (Luna)</a>
       </li>
       <li>
         <a href="/rap/downloads/2.2/">RAP 2.2&emsp;December 2013</a>
