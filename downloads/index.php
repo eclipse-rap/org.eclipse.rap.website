648c9ecbb4d2acabe81a730657dacd7021d8ab33
Markus Knauer <mknauer@eclipsesource.com>
2014-09-26 09:51:12 +0200

    Revert "Release RAP 2.3.1 (Eclipse Luna SR1)"
    This reverts commit 59a3d7ee0ce7f3d995186d14f3d1e73c13a4a226.

--- downloads/index.php
+++ downloads/index.php
@@ -9,7 +9,7 @@
 
   // --- TO BE CHANGED WITH EVERY RELEASE ---
 
-  $releaseBuilds = new Builds( "2.3.1" );
+  $releaseBuilds = new Builds( "2.3" );
   $stableBuilds = new Builds( "3.0" );
 
   // ---
