99146de3db4c5032fc57f0180ef1efa72ff57669
Ralf Sternberg <rsternberg@eclipsesource.com>
2014-10-03 11:05:21 +0200

    Remove attic releases from arrivals sidebar

--- _sidebar/arrivals.php
+++ _sidebar/arrivals.php
@@ -7,18 +7,7 @@
     <div class="terminal-row">
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
-        <span class="terminal-text">RAP 2.2</span>
-      </span>
-      <span class="terminal-field">
-        <span class="terminal-overlay"></span>
-        <span class="terminal-text terminal-arrived">ARRIVED</span>
-      </span>
-    </div>
-
-    <div class="terminal-row">
-      <span class="terminal-field">
-        <span class="terminal-overlay"></span>
-        <span class="terminal-text">RAP 2.1.2</span>
+        <span class="terminal-text">RAP 2.3.1</span>
       </span>
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
@@ -30,17 +19,6 @@
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
         <span class="terminal-text">RAP 3.0 M1</span>
-      </span>
-      <span class="terminal-field">
-        <span class="terminal-overlay"></span>
-        <span class="terminal-text terminal-arrived">ARRIVED</span>
-      </span>
-    </div>
-
-    <div class="terminal-row">
-      <span class="terminal-field">
-        <span class="terminal-overlay"></span>
-        <span class="terminal-text">RAP 2.3.1</span>
       </span>
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
