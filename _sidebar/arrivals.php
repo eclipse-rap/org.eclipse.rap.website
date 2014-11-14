20cc7c90d90facff8f9a799ed29a7853f65e25ed
Ralf Sternberg <rsternberg@eclipsesource.com>
2014-11-14 10:42:51 +0100

    Publish 3.0 M3

--- _sidebar/arrivals.php
+++ _sidebar/arrivals.php
@@ -18,33 +18,11 @@
     <div class="terminal-row">
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
-        <span class="terminal-text">RAP 3.0 M1</span>
-      </span>
-      <span class="terminal-field">
-        <span class="terminal-overlay"></span>
-        <span class="terminal-text terminal-arrived">ARRIVED</span>
-      </span>
-    </div>
-
-    <div class="terminal-row">
-      <span class="terminal-field">
-        <span class="terminal-overlay"></span>
-        <span class="terminal-text">RAP 3.0 M2</span>
-      </span>
-      <span class="terminal-field">
-        <span class="terminal-overlay"></span>
-        <span class="terminal-text terminal-arrived">ARRIVED</span>
-      </span>
-    </div>
-
-    <div class="terminal-row">
-      <span class="terminal-field">
-        <span class="terminal-overlay"></span>
         <span class="terminal-text">RAP 3.0 M3</span>
       </span>
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
-        <span class="terminal-text">NOV 14</span>
+        <span class="terminal-text terminal-arrived">ARRIVED</span>
       </span>
     </div>
 
--- noteworthy/index.php
+++ noteworthy/index.php
@@ -1,5 +1,5 @@
 <?php
 
-  header( "Location: /rap/noteworthy/3.0/?build=M2" );
+  header( "Location: /rap/noteworthy/3.0/?build=M3" );
 
 ?>
