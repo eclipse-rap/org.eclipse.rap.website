d687af46892c6eea03c1150246b95033e7b4ec7f
Ralf Sternberg <rsternberg@eclipsesource.com>
2015-05-22 10:19:15 +0200

    Publish RAP 3.0 RC1

--- _sidebar/arrivals.php
+++ _sidebar/arrivals.php
@@ -44,7 +44,29 @@
       </span>
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
-        <span class="terminal-text">MAY 22</span>
+        <span class="terminal-text terminal-arrived">ARRIVED</span>
+      </span>
+    </div>
+
+    <div class="terminal-row">
+      <span class="terminal-field">
+        <span class="terminal-overlay"></span>
+        <span class="terminal-text">RAP 3.0 RC2</span>
+      </span>
+      <span class="terminal-field">
+        <span class="terminal-overlay"></span>
+        <span class="terminal-text">MAY 29</span>
+      </span>
+    </div>
+
+    <div class="terminal-row">
+      <span class="terminal-field">
+        <span class="terminal-overlay"></span>
+        <span class="terminal-text">RAP 3.0 RC3</span>
+      </span>
+      <span class="terminal-field">
+        <span class="terminal-overlay"></span>
+        <span class="terminal-text">JUN 5</span>
       </span>
     </div>
 
