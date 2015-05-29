09438ec601b7349379c11740cac7b9b5bdf70401
Ralf Sternberg <rsternberg@eclipsesource.com>
2015-05-29 11:10:26 +0200

    Update arrivals page for RAP 3.0 RC2

--- _sidebar/arrivals.php
+++ _sidebar/arrivals.php
@@ -18,17 +18,6 @@
     <div class="terminal-row">
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
-        <span class="terminal-text">RAP 3.0 M6</span>
-      </span>
-      <span class="terminal-field">
-        <span class="terminal-overlay"></span>
-        <span class="terminal-text terminal-arrived">ARRIVED</span>
-      </span>
-    </div>
-
-    <div class="terminal-row">
-      <span class="terminal-field">
-        <span class="terminal-overlay"></span>
         <span class="terminal-text">RAP 3.0 M7</span>
       </span>
       <span class="terminal-field">
@@ -55,7 +44,7 @@
       </span>
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
-        <span class="terminal-text">MAY 29</span>
+        <span class="terminal-text terminal-arrived">ARRIVED</span>
       </span>
     </div>
 
