327e396af8cb4a5c299db593edd63de9b7b713d8
Ralf Sternberg <rsternberg@eclipsesource.com>
2015-08-14 12:05:25 +0200

    Add dates for 3.1 and 3.0 SRs to build.xml, update arrivals section

--- _sidebar/arrivals.php
+++ _sidebar/arrivals.php
@@ -26,6 +26,28 @@
       </span>
     </div>
 
+    <div class="terminal-row">
+      <span class="terminal-field">
+        <span class="terminal-overlay"></span>
+        <span class="terminal-text">RAP 3.1 M1</span>
+      </span>
+      <span class="terminal-field">
+        <span class="terminal-overlay"></span>
+        <span class="terminal-text">AUG 21</span>
+      </span>
+    </div>
+
+    <div class="terminal-row">
+      <span class="terminal-field">
+        <span class="terminal-overlay"></span>
+        <span class="terminal-text">RAP 3.0.1</span>
+      </span>
+      <span class="terminal-field">
+        <span class="terminal-overlay"></span>
+        <span class="terminal-text">SEP 25</span>
+      </span>
+    </div>
+
   </div>
 
 </div>
