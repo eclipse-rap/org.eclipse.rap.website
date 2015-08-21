2c36a8e96d700745c79728c4b51538537b59f053
Ivan Furnadjiev <ivan@eclipsesource.com>
2015-08-21 10:44:42 +0300

    Publish RAP 3.1 M1

--- downloads/index.php
+++ downloads/index.php
@@ -10,7 +10,7 @@
   // --- TO BE CHANGED WITH EVERY RELEASE ---
 
   $releaseBuilds = new Builds( "3.0" );
-  $stableBuilds = new Builds( "3.0" );
+  $stableBuilds = new Builds( "3.1" );
 
   // ---
 
