589c28af199b76e169258a9c7a42dcdccf255ef3
Ivan Furnadjiev <ivan@eclipsesource.com>
2016-05-20 12:10:41 +0300

    Publish 3.1 RC1

--- _sidebar/arrivals.php
+++ _sidebar/arrivals.php
@@ -18,17 +18,6 @@
     <div class="terminal-row">
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
-        <span class="terminal-text">RAP 3.1 M6</span>
-      </span>
-      <span class="terminal-field">
-        <span class="terminal-overlay"></span>
-        <span class="terminal-text terminal-arrived">ARRIVED</span>
-      </span>
-    </div>
-
-    <div class="terminal-row">
-      <span class="terminal-field">
-        <span class="terminal-overlay"></span>
         <span class="terminal-text">RAP 3.1 M7</span>
       </span>
       <span class="terminal-field">
@@ -44,7 +33,18 @@
       </span>
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
-        <span class="terminal-text">MAY 20</span>
+        <span class="terminal-text terminal-arrived">ARRIVED</span>
+      </span>
+    </div>
+
+    <div class="terminal-row">
+      <span class="terminal-field">
+        <span class="terminal-overlay"></span>
+        <span class="terminal-text">RAP 3.1 RC2</span>
+      </span>
+      <span class="terminal-field">
+        <span class="terminal-overlay"></span>
+        <span class="terminal-text">MAY 27</span>
       </span>
     </div>
 
