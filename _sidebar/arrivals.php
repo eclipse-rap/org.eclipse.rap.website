601dd84b6f7bb11d61db4462067336c4f86e4e30
Markus Knauer <mknauer@eclipsesource.com>
2016-05-27 10:53:19 +0200

    Publish 3.1 RC2
    Signed-off-by: Markus Knauer <mknauer@eclipsesource.com>

--- _sidebar/arrivals.php
+++ _sidebar/arrivals.php
@@ -18,17 +18,6 @@
     <div class="terminal-row">
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
-        <span class="terminal-text">RAP 3.1 M7</span>
-      </span>
-      <span class="terminal-field">
-        <span class="terminal-overlay"></span>
-        <span class="terminal-text terminal-arrived">ARRIVED</span>
-      </span>
-    </div>
-
-    <div class="terminal-row">
-      <span class="terminal-field">
-        <span class="terminal-overlay"></span>
         <span class="terminal-text">RAP 3.1 RC1</span>
       </span>
       <span class="terminal-field">
@@ -44,7 +33,18 @@
       </span>
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
-        <span class="terminal-text">MAY 27</span>
+        <span class="terminal-text terminal-arrived">ARRIVED</span>
+      </span>
+    </div>
+
+    <div class="terminal-row">
+      <span class="terminal-field">
+        <span class="terminal-overlay"></span>
+        <span class="terminal-text">RAP 3.1 RC3</span>
+      </span>
+      <span class="terminal-field">
+        <span class="terminal-overlay"></span>
+        <span class="terminal-text">JUN 03</span>
       </span>
     </div>
 
