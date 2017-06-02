efbf5117487091de7557acb948be7deecaf71908
Markus Knauer <mknauer@eclipsesource.com>
2017-06-02 07:58:05 +0200

    Publish RAP 3.2 RC2
    Signed-off-by: Markus Knauer <mknauer@eclipsesource.com>

--- _sidebar/arrivals.php
+++ _sidebar/arrivals.php
@@ -18,22 +18,11 @@
     <div class="terminal-row">
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
-        <span class="terminal-text">RAP 3.2 RC1</span>
-      </span>
-      <span class="terminal-field">
-        <span class="terminal-overlay"></span>
-        <span class="terminal-text terminal-arrived">ARRIVED</span>
-      </span>
-    </div>
-
-    <div class="terminal-row">
-      <span class="terminal-field">
-        <span class="terminal-overlay"></span>
         <span class="terminal-text">RAP 3.2 RC2</span>
       </span>
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
-        <span class="terminal-text">JUN 02</span>
+        <span class="terminal-text terminal-arrived">ARRIVED</span>
       </span>
     </div>
 
