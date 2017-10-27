59987d235d9d82a87c1c02959712e4d1b77d61f0
Markus Knauer <mknauer@eclipsesource.com>
2017-10-27 11:38:19 +0200

    Publish 3.3a (Oxygen.1a fix)
    Signed-off-by: Markus Knauer <mknauer@eclipsesource.com>

--- _sidebar/arrivals.php
+++ _sidebar/arrivals.php
@@ -37,6 +37,28 @@
       </span>
     </div>
 
+    <div class="terminal-row">
+      <span class="terminal-field">
+        <span class="terminal-overlay"></span>
+        <span class="terminal-text">RAP 3.3a</span>
+      </span>
+      <span class="terminal-field">
+        <span class="terminal-overlay"></span>
+        <span class="terminal-text terminal-arrived">ARRIVED</span>
+      </span>
+    </div>
+
+    <div class="terminal-row">
+      <span class="terminal-field">
+        <span class="terminal-overlay"></span>
+        <span class="terminal-text">RAP 3.4 M1</span>
+      </span>
+      <span class="terminal-field">
+        <span class="terminal-overlay"></span>
+        <span class="terminal-text">NOV 03</span>
+      </span>
+    </div>
+
   </div>
 
 </div>
