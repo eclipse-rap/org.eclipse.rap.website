9b8c3f9dd2fd31f242823a716d727ff75fe1b1c5
Markus Knauer <mknauer@eclipsesource.com>
2018-04-14 18:58:54 +0200

    Oxygen.3a contribution and download

--- _sidebar/arrivals.php
+++ _sidebar/arrivals.php
@@ -18,7 +18,7 @@
     <div class="terminal-row">
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
-        <span class="terminal-text">RAP 3.4 SR1</span>
+        <span class="terminal-text">RAP 3.4 SR2</span>
       </span>
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
@@ -34,17 +34,6 @@
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
         <span class="terminal-text terminal-arrived">ARRIVED</span>
-      </span>
-    </div>
-
-    <div class="terminal-row">
-      <span class="terminal-field">
-        <span class="terminal-overlay"></span>
-        <span class="terminal-text">RAP 3.4 SR2</span>
-      </span>
-      <span class="terminal-field">
-        <span class="terminal-overlay"></span>
-        <span class="terminal-text">APR 11</span>
       </span>
     </div>
 
