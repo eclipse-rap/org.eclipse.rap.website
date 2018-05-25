76cd84d74ec11341b7a5b3f15a4b43192d141cac
Markus Knauer <mknauer@eclipsesource.com>
2018-05-25 11:20:16 +0200

    Publish RAP 3.5RC1
    Signed-off-by: Markus Knauer <mknauer@eclipsesource.com>

--- _sidebar/arrivals.php
+++ _sidebar/arrivals.php
@@ -29,7 +29,7 @@
     <div class="terminal-row">
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
-        <span class="terminal-text">RAP 3.5 M2</span>
+        <span class="terminal-text">RAP 3.5 RC1</span>
       </span>
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
@@ -40,11 +40,11 @@
     <div class="terminal-row">
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
-        <span class="terminal-text">RAP 3.5 M3</span>
+        <span class="terminal-text">RAP 3.5 RC2</span>
       </span>
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
-        <span class="terminal-text terminal-arrived">ARRIVED</span>
+        <span class="terminal-text">MAY 31</span>
       </span>
     </div>
 
