3030d6b2990fe59b239c5bc13e581e0967ac1269
Markus Knauer <mknauer@eclipsesource.com>
2018-08-31 18:22:00 +0200

    Publish RAP 3.6 M3
    Change-Id: I0c7d16e44774e47bcd32ec180daa2d7fb4eb300b
    Signed-off-by: Markus Knauer <mknauer@eclipsesource.com>

--- _sidebar/arrivals.php
+++ _sidebar/arrivals.php
@@ -29,7 +29,7 @@
     <div class="terminal-row">
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
-        <span class="terminal-text">RAP 3.6 M2</span>
+        <span class="terminal-text">RAP 3.6 M3</span>
       </span>
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
@@ -40,11 +40,11 @@
     <div class="terminal-row">
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
-        <span class="terminal-text">RAP 3.6 M3</span>
+        <span class="terminal-text">RAP 3.6 RC1</span>
       </span>
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
-        <span class="terminal-text">2018-08-31</span>
+        <span class="terminal-text">2018-09-07</span>
       </span>
     </div>
 
--- _sidebar/contributors.php
+++ _sidebar/contributors.php
@@ -9,12 +9,6 @@
       <a href="http://www.ars.de/" target="_blank"
           title="ARS"><img src="/rap/images/logos/ARS.png"
           style="position: absolute; left: 25px; top: 75px;"/></a>
-      <a href="http://www.cas-crm.com/"
-          target="_blank" title="CAS"><img src="/rap/images/logos/CAS.png"
-          style="position: absolute; left: 145px; top: 75px;"/></a>
-      <a href="http://oneandone.com" target="_blank"
-          title="1&amp;1"><img src="/rap/images/logos/1_1.png"
-          style="position: absolute; left: 25px; top: 141px;"/></a>
       <a href="http://www.uid.com" target="_blank"
           title="UID"><img src="/rap/images/logos/UID.png"
           style="position: absolute; left: 130px; top: 190px;"/></a>
