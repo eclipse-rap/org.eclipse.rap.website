396f3d0bc23a76bc3cf6367352dfaeba9d54e3ae
Markus Knauer <mknauer@eclipsesource.com>
2019-02-08 10:25:00 +0100

    Publish RAP 3.8 M2
    Change-Id: Ib95a80d268a0538412b838645629c16001d3252f
    Signed-off-by: Markus Knauer <mknauer@eclipsesource.com>

--- _sidebar/arrivals.php
+++ _sidebar/arrivals.php
@@ -29,7 +29,7 @@
     <div class="terminal-row">
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
-        <span class="terminal-text">RAP 3.8 M1</span>
+        <span class="terminal-text">RAP 3.8 M2</span>
       </span>
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
@@ -40,11 +40,11 @@
     <div class="terminal-row">
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
-        <span class="terminal-text">RAP 3.8 M2</span>
+        <span class="terminal-text">RAP 3.8 M3</span>
       </span>
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
-        <span class="terminal-text">2019-02-08</span>
+        <span class="terminal-text">2019-03-01</span>
       </span>
     </div>
 
