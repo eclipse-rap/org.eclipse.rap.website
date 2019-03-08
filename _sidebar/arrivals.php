7e2546f729c9eac43ac36833ce9e0077ab4213af
Markus Knauer <mknauer@eclipsesource.com>
2019-03-08 07:18:50 +0100

    Publish RAP 3.8 RC1
    Change-Id: I01ad72527dc0f7c5413a784b1ada29e5d1734e0f
    Signed-off-by: Markus Knauer <mknauer@eclipsesource.com>

--- _sidebar/arrivals.php
+++ _sidebar/arrivals.php
@@ -29,7 +29,7 @@
     <div class="terminal-row">
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
-        <span class="terminal-text">RAP 3.8 M3</span>
+        <span class="terminal-text">RAP 3.8 RC1</span>
       </span>
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
@@ -40,11 +40,11 @@
     <div class="terminal-row">
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
-        <span class="terminal-text">RAP 3.8 RC1</span>
+        <span class="terminal-text">RAP 3.8 RC2</span>
       </span>
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
-        <span class="terminal-text">2019-03-08</span>
+        <span class="terminal-text">2019-03-15</span>
       </span>
     </div>
 
