278569c0e762b6df882047c84162445da2cbc9e1
Markus Knauer <mknauer@eclipsesource.com>
2019-04-19 11:48:26 +0200

    Publish RAP 3.9.0 M1
    Change-Id: Icb280c262743d2fe6ca025ba1b8b3dabffeabb8d
    Signed-off-by: Markus Knauer <mknauer@eclipsesource.com>

--- _sidebar/arrivals.php
+++ _sidebar/arrivals.php
@@ -55,7 +55,18 @@
       </span>
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
-        <span class="terminal-text">2019-04-19</span>
+        <span class="terminal-text terminal-arrived">ARRIVED</span>
+      </span>
+    </div>
+
+    <div class="terminal-row">
+      <span class="terminal-field">
+        <span class="terminal-overlay"></span>
+        <span class="terminal-text">RAP 3.9 M2</span>
+      </span>
+      <span class="terminal-field">
+        <span class="terminal-overlay"></span>
+        <span class="terminal-text">2019-05-10</span>
       </span>
     </div>
 
--- developers-guide/DevGuideUtils.php
+++ developers-guide/DevGuideUtils.php
@@ -3,7 +3,7 @@
 class DevGuideUtils {
 
   const CURRENT_VERSION = '3.8';
-  const MASTER_VERSION = '3.8';
+  const MASTER_VERSION = '3.9';
 
   public static $versions = array(
     '2.0' => array(
@@ -75,6 +75,12 @@ class DevGuideUtils {
     '3.8' => array(
       'rootUrl' => 'http://download.eclipse.org/rt/rap/doc/3.8/',
       'apiUrl' => 'http://download.eclipse.org/rt/rap/doc/3.8/guide/reference/',
+      'tocPath' => 'guide/toc.xml',
+      'topicPath' => 'guide/articles/'
+    ),
+    '3.9' => array(
+      'rootUrl' => 'http://download.eclipse.org/rt/rap/doc/3.9/',
+      'apiUrl' => 'http://download.eclipse.org/rt/rap/doc/3.9/guide/reference/',
       'tocPath' => 'guide/toc.xml',
       'topicPath' => 'guide/articles/'
     )
