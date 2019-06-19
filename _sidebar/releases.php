e39c15a3a2c5f7feb97691c8139c0199e0908616
Markus Knauer <mknauer@eclipsesource.com>
2019-06-19 10:41:26 +0200

    Add 3.9 release to releases sidebar listing
    Change-Id: I1d16a9b6d5fcbe3d772c57f2185ae177fac1f8a6
    Signed-off-by: Markus Knauer <mknauer@eclipsesource.com>

--- _sidebar/releases.php
+++ _sidebar/releases.php
@@ -5,6 +5,9 @@
   <div class="sideitemcontent">
     <ul>
       <li>
+        <a href="/rap/downloads/3.8/">RAP 3.9&emsp;June 2019 (2019-06)</a>
+      </li>
+      <li>
         <a href="/rap/downloads/3.8/">RAP 3.8&emsp;March 2019 (2019-03)</a>
       </li>
       <li>
