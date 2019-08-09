56912e8d2f389f6993bf53087365deb3cc7ac0c2
Markus Knauer <mknauer@eclipsesource.com>
2019-08-09 06:24:54 +0200

    Publish RAP 3.10.0 M2
    Change-Id: Id51474b4ed60a67eb6fac48397d74dcbcc676bf7

--- _sidebar/arrivals.php
+++ _sidebar/arrivals.php
@@ -29,7 +29,7 @@
     <div class="terminal-row">
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
-        <span class="terminal-text">RAP 3.10 M1</span>
+        <span class="terminal-text">RAP 3.10 M2</span>
       </span>
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
@@ -40,11 +40,11 @@
     <div class="terminal-row">
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
-        <span class="terminal-text">RAP 3.10 M2</span>
+        <span class="terminal-text">RAP 3.10 M3</span>
       </span>
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
-        <span class="terminal-text">2019-08-09</span>
+        <span class="terminal-text">2019-08-30</span>
       </span>
     </div>
 
