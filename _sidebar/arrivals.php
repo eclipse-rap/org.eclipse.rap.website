4f10d04f262ffcde2c3b3deb837fc24b90ede42d
Markus Knauer <mknauer@eclipsesource.com>
2020-08-28 10:46:09 +0200

    Publish RAP 3.14.0 M3
    Change-Id: I5538b797fb4d5a1cd58c168ec018e7cf8133d906
    Signed-off-by: Markus Knauer <mknauer@eclipsesource.com>

--- _sidebar/arrivals.php
+++ _sidebar/arrivals.php
@@ -29,7 +29,7 @@
     <div class="terminal-row">
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
-        <span class="terminal-text">RAP 3.14 M2</span>
+        <span class="terminal-text">RAP 3.14 M3</span>
       </span>
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
@@ -40,11 +40,11 @@
     <div class="terminal-row">
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
-        <span class="terminal-text">RAP 3.14 M3</span>
+        <span class="terminal-text">RAP 3.14 RC1</span>
       </span>
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
-        <span class="terminal-text">2020-08-28</span>
+        <span class="terminal-text">2020-09-04</span>
       </span>
     </div>
 
