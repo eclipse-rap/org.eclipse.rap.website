e3e62ec5fa3f2fb2e7852b2755747dba5a96bcb4
Markus Knauer <mknauer@eclipsesource.com>
2021-06-04 10:18:20 +0200

    Publish RAP 3.17.0 RC1
    Change-Id: I9477c09dfd34f261159d56853974d931de88e393
    Signed-off-by: Markus Knauer <mknauer@eclipsesource.com>

--- _sidebar/arrivals.php
+++ _sidebar/arrivals.php
@@ -29,11 +29,11 @@
     <div class="terminal-row">
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
-        <span class="terminal-text">RAP 3.17 M3</span>
+        <span class="terminal-text">RAP 3.17 RC1</span>
       </span>
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
-        <span class="terminal-text">2021-05-28</span>
+        <span class="terminal-text">2021-06-04</span>
       </span>
     </div>
 
