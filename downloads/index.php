7ef52f88ae1033f335bd8ac4f59fbacf254d3789
Markus Knauer <mknauer@eclipsesource.com>
2021-09-16 15:15:44 +0200

    Revert stable download release to 3.18
    Change-Id: I4af186a78230fcc4794f714c136839b92a55adc9
    Signed-off-by: Markus Knauer <mknauer@eclipsesource.com>

--- downloads/index.php
+++ downloads/index.php
@@ -10,7 +10,7 @@
   // --- TO BE CHANGED WITH EVERY RELEASE ---
 
   $releaseBuilds = new Builds( "3.18" );
-  $stableBuilds = new Builds( "3.19" );
+  $stableBuilds = new Builds( "3.18" );
 
   // ---
 
