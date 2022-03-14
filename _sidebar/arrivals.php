b6233522b8587e9f5ae481cc22f0616dc3d20172
Markus Knauer <mknauer@eclipsesource.com>
2022-03-14 16:42:08 +0100

    Publish RAP 3.20.0 RC2
    Change-Id: Ia380e05d41400446a14c2c728426adadff46d412

--- _sidebar/arrivals.php
+++ _sidebar/arrivals.php
@@ -29,22 +29,11 @@
     <div class="terminal-row">
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
-        <span class="terminal-text">RAP 3.20 M1</span>
+        <span class="terminal-text">RAP 3.20 RC2</span>
       </span>
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
         <span class="terminal-text terminal-arrived">ARRIVED</span>
-      </span>
-    </div>
-
-    <div class="terminal-row">
-      <span class="terminal-field">
-        <span class="terminal-overlay"></span>
-        <span class="terminal-text">RAP 3.20 M2</span>
-      </span>
-      <span class="terminal-field">
-        <span class="terminal-overlay"></span>
-        <span class="terminal-text">2022-02-04</span>
       </span>
     </div>
 
