64b3fb0895ccc6c95a36d8acb30ddea6fc813994
Markus Knauer <mknauer@eclipsesource.com>
2022-06-08 12:00:00 +0000

    Publish RAP Runtime 3.21 RC1
    Change-Id: Iaa887bb890f2c94e02ad67126c21eafa0b09e32d
    Signed-off-by: Markus Knauer <mknauer@eclipsesource.com>

--- _sidebar/arrivals.php
+++ _sidebar/arrivals.php
@@ -29,7 +29,7 @@
     <div class="terminal-row">
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
-        <span class="terminal-text">RAP 3.21 M3</span>
+        <span class="terminal-text">RAP 3.21 RC1</span>
       </span>
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
@@ -40,11 +40,11 @@
     <div class="terminal-row">
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
-        <span class="terminal-text">RAP 3.21 RC1</span>
+        <span class="terminal-text">RAP 3.21 RC2</span>
       </span>
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
-        <span class="terminal-text">2022-06-03</span>
+        <span class="terminal-text">2022-06-10</span>
       </span>
     </div>
 
