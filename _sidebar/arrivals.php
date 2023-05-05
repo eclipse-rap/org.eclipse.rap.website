407ae0651db5baf1ddf6fd0cbfa15ce4d8631f3b
Markus Knauer <mknauer@eclipsesource.com>
2023-05-05 12:00:00 +0000

    Publish RAP Runtime 3.25 M2

--- _sidebar/arrivals.php
+++ _sidebar/arrivals.php
@@ -29,7 +29,7 @@
     <div class="terminal-row">
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
-        <span class="terminal-text">RAP 3.25 M1</span>
+        <span class="terminal-text">RAP 3.25 M2</span>
       </span>
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
@@ -40,11 +40,11 @@
     <div class="terminal-row">
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
-        <span class="terminal-text">RAP 3.25 M2</span>
+        <span class="terminal-text">RAP 3.25 M3</span>
       </span>
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
-        <span class="terminal-text">2023-05-05</span>
+        <span class="terminal-text">2023-05-26</span>
       </span>
     </div>
 
