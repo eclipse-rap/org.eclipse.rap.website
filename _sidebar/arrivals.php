88d95bae1a4e0e0d6bbfb5b17455cb16d96db7be
Markus Knauer <mknauer@eclipsesource.com>
2023-11-24 08:27:07 +0100

    Publish RAP Runtime 3.27 RC1

--- _sidebar/arrivals.php
+++ _sidebar/arrivals.php
@@ -29,7 +29,7 @@
     <div class="terminal-row">
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
-        <span class="terminal-text">RAP 3.27 M3</span>
+        <span class="terminal-text">RAP 3.27 RC1</span>
       </span>
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
@@ -40,11 +40,11 @@
     <div class="terminal-row">
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
-        <span class="terminal-text">RAP 3.27 RC1</span>
+        <span class="terminal-text">RAP 3.27 RC2</span>
       </span>
       <span class="terminal-field">
         <span class="terminal-overlay"></span>
-        <span class="terminal-text">2023-11-24</span>
+        <span class="terminal-text">2023-12-01</span>
       </span>
     </div>
 
